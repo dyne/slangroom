8c3bc20ac750c93d64462d773f4a11ad07227c7c
srfsh <dev@srf.sh>
2023-07-06 20:08:27 +0300

    test(fs/visitor): add more tests

--- pkg/fs/visitor.test.ts
+++ pkg/fs/visitor.test.ts
@@ -1,6 +1,6 @@
 import { visit } from './visitor';
 
-test('that', async () => {
+test('ast is correct', async () => {
 	// Given I have a contract with filesystem statements in it
 	const contract = `Rule unknown ignore
 Given I have a 'string' named 'stringToWrite'
@@ -8,9 +8,18 @@ and I have a 'string' named 'nameOfTheFile'
 
 Then I save the 'stringToWrite' into the file 'nameOfTheFile'
 `;
-	await visit(contract, {
-		data: { stringToWrite: 'hello world', nameOfTheFile: 'hello-world.txt' },
-	});
-
-	expect(true).toStrictEqual(true);
+	// When I generate ast of the contract
+	const data = {
+		stringToWrite: 'hello world',
+		nameOfTheFile: 'hello-world.txt',
+	};
+	const ast = await visit(contract, { data: data });
+	// Then the content must be "stringToWrite"
+	expect(ast.content).toStrictEqual('stringToWrite');
+	// and the filename must be "nameOfTheFile"
+	expect(ast.filename).toStrictEqual('nameOfTheFile');
+	// and the value indexed by the content in data must be "hello world"
+	expect(data[ast.content as 'stringToWrite']).toStrictEqual('hello world');
+	// and the value indexed by the filename in data must be "hello-world.txt"
+	expect(data[ast.filename as 'nameOfTheFile']).toStrictEqual('hello-world.txt');
 });
