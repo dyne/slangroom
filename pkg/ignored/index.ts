d17f869b280ab68a64cc29b64cc10f8d5c8edcbc
srfsh <dev@srf.sh>
2023-07-16 16:41:52 +0300

    doc(ignored): clarify usage
    Clarify the usage of getIgnoredStatements().

--- pkg/ignored/index.ts
+++ pkg/ignored/index.ts
@@ -8,6 +8,9 @@ const IgnoredLexer = new Lexer(vocab);
 /**
  * Finds statements ignored by Zenroom in the provided contract.
  *
+ * If no statement is found, then that means the contract was executed without
+ * any statements found.
+ *
  * @param contract is the Zenroom contract.
  * @param params is the parameters of Zenroom, such as data and keys.
  * @returns the ignored statements.
