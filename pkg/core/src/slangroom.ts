397a1e77ce22b9843004635f8fb6b05c55e0e671
Alberto Lerda <alberto@dyne.org>
2023-09-25 23:58:46 +0100

    refactor parsing

--- pkg/core/src/slangroom.ts
+++ pkg/core/src/slangroom.ts
@@ -1,87 +1,257 @@
-import { BeforePlugin, AfterPlugin } from '@slangroom/core/plugin';
-import { type ZenroomParams, type ZenroomOutput, zencodeExec } from '@slangroom/shared';
-import { getIgnoredStatements } from '@slangroom/ignored';
+import { createToken, Lexer, CstParser } from "@slangroom/deps/chevrotain";
+//import { createSyntaxDiagramsCode } from "chevrotain";
 
-/**
- * A helper type that disallows nested arrays.
- */
-type Plugins =
-	| BeforePlugin
-	| AfterPlugin
-	| Set<BeforePlugin | AfterPlugin>
-	| Array<BeforePlugin | AfterPlugin | Set<BeforePlugin | AfterPlugin>>;
-
-/**
- * A Slangroom instance.
- */
-export class Slangroom {
-	/**
-	 * A set of plugins that needs to be executed **before** the actual Zenroom execution.
-	 */
-	private _beforeExecution = new Set<BeforePlugin>();
-	get beforeExecution() {
-		return this._beforeExecution;
-	}
-
-	/**
-	 * A set of plugins that needs to be executed **after** the actual Zenroom execution.
-	 */
-	private _afterExecution = new Set<AfterPlugin>();
-	get afterExecution() {
-		return this._afterExecution;
-	}
-
-	constructor(first: Plugins, ...rest: Plugins[]) {
-		this.addPlugins(first, ...rest);
-	}
-
-	/**
-	 * Adds a single or a list of plugins to the Slangroom instance.
-	 */
-	addPlugins(first: Plugins, ...rest: Plugins[]) {
-		const plugins = new Set<BeforePlugin | AfterPlugin>();
-		[first, ...rest].forEach(function recurse(x: Plugins) {
-			if (Array.isArray(x) || x instanceof Set) x.forEach(recurse);
-			else plugins.add(x);
-		});
-
-		for (const p of plugins) {
-			if (p instanceof BeforePlugin) this._beforeExecution.add(p);
-			if (p instanceof AfterPlugin) this._afterExecution.add(p);
-		}
-	}
-
-	/**
-	 * Executes a contract using optional parameters with custom statements.
-	 */
-	async execute(contract: string, params?: ZenroomParams): Promise<ZenroomOutput> {
-		const ignoreds = await getIgnoredStatements(contract, params);
-		params = params || {data: {}}
-
-		// TODO: remove the statements when they match (decide how)
-		for (const ignored of ignoreds) {
-			for (const b of this._beforeExecution) {
-				const res = await b.execute({
-					statement: ignored,
-					params: params,
-				})
-				params.data = Object.assign(params.data || {}, res)
-			}
-		}
-
-		const zout = await zencodeExec(contract, params);
-
-		for (const ignored of ignoreds) {
-			for (const a of this._afterExecution) {
-				const res = await a.execute({
-					statement: ignored,
-					result: zout.result,
-					params: params,
-				})
-				zout.result = Object.assign(zout.result || {}, res)
-			}
-		}
-
-		return zout;
-	}
+type Action = {
+  clause: string,
+  args: string[]
 }
+
+type IntoStmt = {
+  kind: "Into";
+  read: Action;
+  result: string
+};
+type AndStmt = {
+  kind: "And";
+  read?: Action;
+  save?: Action;
+};
+type Stmt =
+  | IntoStmt
+  | AndStmt;
+
+
+const stmtJoin = (s1: Stmt, s2: Stmt): AndStmt => {
+  if(s1.kind != "And" || s2.kind != "And") {
+    throw new Error("Can only join And statements");
+  }
+  const s3: AndStmt = s1
+  if (s2.read) {
+    s3.read = s2.read;
+  }
+  if (s2.save) {
+    s1.save = s2.save;
+  }
+  return s3;
+}
+// A plugin will be (VERB, ACTION)
+const Save = createToken({
+  name: "Save",
+  pattern: /save/i,
+});
+const Read = createToken({
+  name: "Read",
+  pattern: /read/i,
+});
+
+// Top level keywords
+const And = createToken({
+  name: "And",
+  pattern: /and/i,
+});
+const Into = createToken({
+  name: "Into",
+  pattern: /into/i,
+});
+
+
+
+// Actions
+const Keyword = createToken({
+  name: "Keyword",
+  pattern: /[a-z]+/i,
+});
+
+const Identifier = createToken({
+  name: "Identifier",
+  pattern: /'[a-z]+'/i,
+});
+
+
+const WhiteSpace = createToken({
+  name: "WhiteSpace",
+  pattern: /\s+/,
+  group: Lexer.SKIPPED,
+});
+
+const allTokens = [
+  WhiteSpace,
+  Read,
+  Save,
+  Into,
+  And,
+  Identifier,
+  Keyword,
+];
+const StatementLexer = new Lexer(allTokens);
+// ----------------- parser -----------------
+class StatementParser extends CstParser {
+  constructor() {
+    super(allTokens);
+
+    this.performSelfAnalysis();
+  }
+
+  public readClause = this.RULE("readClause", () => {
+    this.CONSUME(Read);
+    this.SUBRULE(this.customs)
+    this.OR2([
+        { ALT: () => {
+        this.CONSUME(Into);
+        this.CONSUME(Identifier);
+        } },
+        { ALT: () => {
+        this.CONSUME(And);
+        this.SUBRULE(this.statement);
+        } },
+        { ALT: () => { } },
+    ]);
+  });
+  public saveClause = this.RULE("saveClause", () => {
+    this.CONSUME(Save);
+    this.SUBRULE(this.customs)
+    this.OR2([
+        { ALT: () => {
+        this.CONSUME(And);
+        this.SUBRULE(this.statement);
+        } },
+        { ALT: () => { } },
+    ]);
+  });
+  public statement = this.RULE("statement", () => {
+    this.OR([{ ALT: () => { this.SUBRULE(this.readClause); }},
+             { ALT: () => { this.SUBRULE(this.saveClause); }},
+    ]);
+  });
+
+
+  // We will replace this with actions
+  // Restroom-like match
+  public customs = this.RULE("customs", () => {
+    this.MANY(() => {
+      this.SUBRULE(this.custom)
+    })
+  });
+
+  public custom = this.RULE("custom", () => {
+    this.OR([
+      {ALT:()=>{
+        this.CONSUME(Identifier)
+      }},
+      {ALT:()=>{
+        this.CONSUME(Keyword)
+      }},
+    ])
+  });
+
+}
+
+// wrapping it all together
+// reuse the same parser instance.
+const parser = new StatementParser();
+// ----------------- Interpreter -----------------
+// Obtains the default CstVisitor constructor to extend.
+const BaseCstVisitor = parser.getBaseCstVisitorConstructor();
+// All our semantics go into the visitor, completly separated from the grammar.
+
+const normalizeStatement = (stmt: string) => {
+  return stmt.replaceAll(/'[^']*'/gi, "''")
+}
+
+const getArgs = (stmt: string) => {
+    return stmt.match(/'([^']+)'/g)?.map(match => match.slice(1, -1)) || [];
+}
+
+class StatementInterpreter extends BaseCstVisitor {
+
+  constructor() {
+    super();
+    this.validateVisitor();
+  }
+
+  statement(ctx: any) {
+    let res: Stmt = {
+      kind: "And",
+    }
+    if(ctx.readClause) {
+      res = this.visit(ctx.readClause)
+    } else if(ctx.saveClause) {
+      res = this.visit(ctx.saveClause)
+    }
+
+    return res
+  }
+
+  readClause(ctx: any) {
+    const action = this.visit(ctx.customs)
+    if(ctx.Into) {
+      // simple statement just read and write to a variable
+      return {
+        kind: "Into",
+        read: action,
+        result: ctx.Identifier[0].image.slice(1,-1),
+      }
+    }
+    let res: AndStmt =  {
+      kind: "And",
+      read: action
+    }
+    if(ctx.statement) {
+      const other = this.visit(ctx.statement)
+      res = stmtJoin(res, other)
+    }
+    return res
+  }
+
+  saveClause(ctx: any) {
+    const action = this.visit(ctx.customs)
+    let res: AndStmt =  {
+      kind: "And",
+      save: action
+    }
+    if(ctx.statement) {
+      res = stmtJoin(res, this.visit(ctx.statement))
+    }
+    return res
+  }
+
+  customs(ctx: any) {
+    const action = ctx.custom.map((v: any[]) => this.visit(v)).join(' ')
+    const normalized = normalizeStatement(action)
+    const args = getArgs(action)
+    return {
+      clause: normalized,
+      args
+    }
+  }
+
+  custom(ctx: any) {
+    return (ctx.Keyword || ctx.Identifier)[0].image
+  }
+
+
+}
+
+// We only need a single interpreter instance because our interpreter has no state.
+const interpreter = new StatementInterpreter();
+
+export const line2AST = (text: string) => {
+  // 1. Tokenize the input.
+  const lexResult = StatementLexer.tokenize(text);
+  //console.log(lexResult)
+
+  // 2. Parse the Tokens vector.
+  parser.input = lexResult.tokens;
+  const cst = parser.statement();
+
+  // 3. Perform semantics using a CstVisitor.
+  // Note that separation of concerns between the syntactic analysis (parsing) and the semantics.
+  const value = interpreter.visit(cst);
+
+  return {
+    value: value,
+    lexResult: lexResult,
+    parseErrors: parser.errors,
+  };
+}
+
--- pkg/core/test/slangroom.ts
+++ pkg/core/test/slangroom.ts
@@ -1,141 +1,22 @@
 import test from 'ava';
-import { BeforePlugin, AfterPlugin, Slangroom } from '@slangroom/core';
+import { line2AST } from '@slangroom/core';
+line2AST;
 
-test('adding a plugin correctly falls into either before or after', (t) => {
-	const before = new BeforePlugin(() => {
-		return;
-	});
-	const after = new AfterPlugin(() => {
-		return;
-	});
-	const slang = new Slangroom(before, after);
-
-	t.is(slang.beforeExecution.size, 1);
-	t.true(slang.beforeExecution.has(before));
-
-	t.is(slang.afterExecution.size, 1);
-	t.true(slang.afterExecution.has(after));
-});
-
-test('no plugins are executed if no ignored statemnets are found', async (t) => {
-	let hasBeforeRan = false;
-	let hasAfterRan = false;
-	const before = new BeforePlugin(() => {
-		hasBeforeRan = true;
-		return;
-	});
-	const after = new BeforePlugin(() => {
-		hasAfterRan = true;
-		return;
-	});
-	const slang = new Slangroom([before, after]);
-	const contract = `Given I have nothing
-Then I print the string 'I love you'
-`;
-	await slang.execute(contract);
-	t.false(hasBeforeRan);
-	t.false(hasAfterRan);
-});
-
-test('before-plugins runs before the actual execution and after-plugins runs after', async (t) => {
-	let hasBeforeRan = false;
-	let hasAfterRan = false;
-	const before = new BeforePlugin(() => {
-		t.false(hasBeforeRan);
-		t.false(hasAfterRan);
-		hasBeforeRan = true;
-		return;
-	});
-	const after = new AfterPlugin(() => {
-		t.true(hasBeforeRan);
-		t.false(hasAfterRan);
-		hasAfterRan = true;
-		return;
-	});
-	const slang = new Slangroom(new Set([before, after]));
-	const contract = `Rule unknown ignore
-
-Given I have nothing
-Then I print the string 'I love you'
-Then this statement does not exist
-`;
-	await slang.execute(contract);
-	t.true(hasBeforeRan);
-	t.true(hasAfterRan);
-});
-
-test('before-plugins can inject parameters', async (t) => {
-	const before = new BeforePlugin(() => {
-		console.log("foobarbra")
-		return {foo: 'bar' };
-	});
-	const slang = new Slangroom(before);
-	const contract = `Rule unknown ignore
-
-Given I have a 'string' named 'foo'
-When I need an ignored statement
-Then I print 'foo'
-`;
-	const zout = await slang.execute(contract);
-	t.is(zout.result['foo'] as string, 'bar');
-});
-
-test('after-plugins can return values', async (t) => {
-	const before = new AfterPlugin(() => {
-		return { foo: "bar" };
-	});
-	const slang = new Slangroom(before);
-	const contract = `Rule unknown ignore
-Given nothing
-Then done
-Then I need an ignored statement
-`;
-	const zout = await slang.execute(contract);
-	t.is(zout.result['foo'] as string, 'bar');
-});
-
-test('check statements order', async (t) => {
-	const beforeA = new BeforePlugin((ctx) => {
-		if(!ctx.params?.data) return
-		if(ctx.statement == "Given A") {
-			t.is(ctx.params?.data['state'], "BEGIN")
-			return {state: "A"}
-		}
-		return
-	});
-	const beforeB = new BeforePlugin((ctx) => {
-		if(!ctx.params?.data) return
-		if(ctx.statement == "Given B") {
-			t.is(ctx.params?.data['state'], "A")
-			return {state: "B"}
-		}
-		return
-	});
-	const afterC = new AfterPlugin((ctx) => {
-		if(!ctx.params?.data) return
-		if(ctx.statement == "Then C") {
-			t.is(ctx.result['state'], "B")
-			return {state: "C"}
-		}
-		return
-	});
-	const afterD = new AfterPlugin((ctx) => {
-		if(!ctx.params?.data) return
-		if(ctx.statement == "Then D") {
-			t.is(ctx.result['state'], "C")
-			return {state: "D"}
-		}
-		return
-	});
-	const slang = new Slangroom(beforeB, beforeA, afterD, afterC);
-	const contract = `Rule unknown ignore
-Given A
-Given B
-Given I have a 'string' named 'state'
-Then print the 'state'
-Then C
-Then D
-`;
-	const zout = await slang.execute(contract, { data: { state: "BEGIN" } });
-	t.is(zout.result['state'] as string, 'D');
-});
+test("Generate correct AST 1", async (t) => {
+	t;
+  const res = line2AST("read the ethereum balance of 'address' and save in the file 'baz'")
+  t.deepEqual(res.value, {
+    "kind":"And",
+    "read":{"clause":"the ethereum balance of ''","args":["address"]},
+    "save":{"clause":"in the file ''","args":["baz"]}
+  })
+})
+test("Generate correct AST 2", async (t) => {
+	t;
+  const res = line2AST("read the ethereum balance of 'address' into 'bar'")
+  t.deepEqual(res.value, {
+    "kind":"Into",
+    "read":{"clause":"the ethereum balance of ''","args":["address"]},
+    "result":"bar"
+  })
+})
