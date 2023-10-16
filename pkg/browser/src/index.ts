4d26706f843beb3ab66b19ba07702d3247630b5d
Alberto Lerda <alberto@dyne.org>
2023-10-16 16:36:45 +0100

    chore: test in the browser with playwright

---
+++ pkg/browser/src/index.ts
@@ -0,0 +1,8 @@
+import { Slangroom } from "@slangroom/core";
+import { httpPlugins } from "@slangroom/http";
+const slangroom = new Slangroom(httpPlugins);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).slangroom = slangroom; // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513740/5433572
+
+console.log('slangroom object initialized');
