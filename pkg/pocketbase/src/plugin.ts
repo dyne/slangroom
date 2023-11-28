c8578115d8053060731f6cb4795fdda63e85f4b2
phoebus-84 <ennio@dyne.org>
2023-11-28 15:11:41 +0100

    fix: dont prettignore everything

--- pkg/pocketbase/src/plugin.ts
+++ pkg/pocketbase/src/plugin.ts
@@ -1,175 +1,194 @@
-import PocketBase, { FullListOptions, ListResult, RecordModel, RecordOptions } from 'pocketbase'
-import { Plugin } from '@slangroom/core'
-import { z } from 'zod'
+import PocketBase, { FullListOptions, ListResult, RecordModel, RecordOptions } from 'pocketbase';
+import { Plugin } from '@slangroom/core';
+import { z } from 'zod';
 
-let pb:PocketBase;
-const p = new Plugin()
+let pb: PocketBase;
+const p = new Plugin();
 
-const serverUrlSchema = z.literal(`http${z.union([z.literal('s'), z.literal('')])}://${z.string()}/`)
-export type ServerUrl = z.infer<typeof serverUrlSchema>
+const serverUrlSchema = z.literal(
+	`http${z.union([z.literal('s'), z.literal('')])}://${z.string()}/`,
+);
+export type ServerUrl = z.infer<typeof serverUrlSchema>;
 
 const credentialsSchema = z.object({
-    email: z.string().email(),
-    password: z.string()
-})
+	email: z.string().email(),
+	password: z.string(),
+});
 
-export type Credentials = z.infer<typeof credentialsSchema> 
+export type Credentials = z.infer<typeof credentialsSchema>;
 
 const baseRecordParametersSchema = z.object({
-    expand: z.string().optional(),
-    requestKey: z.string().optional(),
-    fields: z.string().optional()
-})
+	expand: z.string().optional(),
+	requestKey: z.string().optional(),
+	fields: z.string().optional(),
+});
 
-export type RecordBaseParameters = z.infer<typeof baseRecordParametersSchema>
+export type RecordBaseParameters = z.infer<typeof baseRecordParametersSchema>;
 
-const baseFetchRecordParametersSchema = baseRecordParametersSchema.extend({collection: z.string()})
+const baseFetchRecordParametersSchema = baseRecordParametersSchema.extend({
+	collection: z.string(),
+});
 
 const paginationSchema = z.object({
-    page: z.number().int(),
-    perPage: z.number().int(),
-})
+	page: z.number().int(),
+	perPage: z.number().int(),
+});
 
-const listParametersBaseSchema = z.object({
-    sort: z.string().default('-created').optional(),
-}).merge(baseFetchRecordParametersSchema)
+const listParametersBaseSchema = z
+	.object({
+		sort: z.string().default('-created').optional(),
+	})
+	.merge(baseFetchRecordParametersSchema);
 
-const listParametersSchema = z.discriminatedUnion("type", [
-    listParametersBaseSchema.extend({ type: z.literal('all'), filter: z.string().optional() }),
-    listParametersBaseSchema.extend({ type: z.literal('list'), filter: z.string().optional(), pagination: paginationSchema }),
-    listParametersBaseSchema.extend({ type: z.literal('first'), filter: z.string() })
-])
+const listParametersSchema = z.discriminatedUnion('type', [
+	listParametersBaseSchema.extend({ type: z.literal('all'), filter: z.string().optional() }),
+	listParametersBaseSchema.extend({
+		type: z.literal('list'),
+		filter: z.string().optional(),
+		pagination: paginationSchema,
+	}),
+	listParametersBaseSchema.extend({ type: z.literal('first'), filter: z.string() }),
+]);
 
-export type ListParameters = z.input<typeof listParametersSchema>
+export type ListParameters = z.input<typeof listParametersSchema>;
 
-const showParametersSchema = z.object({
-    id: z.string()
-}).merge(baseFetchRecordParametersSchema)
+const showParametersSchema = z
+	.object({
+		id: z.string(),
+	})
+	.merge(baseFetchRecordParametersSchema);
 
-export type ShowRecordParameters = z.infer<typeof showParametersSchema>
+export type ShowRecordParameters = z.infer<typeof showParametersSchema>;
 
-const createRecordParametersSchema = z.object({
-    record: z.record(z.string(), z.any())
-}).merge(baseFetchRecordParametersSchema)
+const createRecordParametersSchema = z
+	.object({
+		record: z.record(z.string(), z.any()),
+	})
+	.merge(baseFetchRecordParametersSchema);
 
-export type CreateRecordParameters = z.infer<typeof createRecordParametersSchema>
+export type CreateRecordParameters = z.infer<typeof createRecordParametersSchema>;
 
 const isPbRunning = async () => {
-    const res = await pb.health.check({ requestKey: null })
-    if (res.code !== 200) return false
-    return true
-}
+	const res = await pb.health.check({ requestKey: null });
+	if (res.code !== 200) return false;
+	return true;
+};
 
 /**
  * @internal
  */
-export const setupClient = p.new(['pb_address'], 'create pb_client', async (ctx)=> {
-    const address = ctx.fetch('pb_address')
-    if (typeof address !== 'string') return ctx.fail("Invalid address")
-    try {
-        pb = new PocketBase(address)
-        const res = await pb.health.check({ requestKey: null })
-        if (res.code !== 200) ctx.fail("server error")
-        return ctx.pass("pb client successfully created")
-    } catch(e) {
-        return ctx.fail("Invalid address")
-    }
-})
+export const setupClient = p.new(['pb_address'], 'create pb_client', async (ctx) => {
+	const address = ctx.fetch('pb_address');
+	if (typeof address !== 'string') return ctx.fail('Invalid address');
+	try {
+		pb = new PocketBase(address);
+		const res = await pb.health.check({ requestKey: null });
+		if (res.code !== 200) ctx.fail('server error');
+		return ctx.pass('pb client successfully created');
+	} catch (e) {
+		return ctx.fail('Invalid address');
+	}
+});
 
 /**
  * @internal
  */
 export const authWithPassword = p.new(['my_credentials'], 'login', async (ctx) => {
-    const credentials = ctx.fetch('my_credentials') as Credentials
-    
-    const validation = credentialsSchema.safeParse(credentials)
-    if (!validation.success) return ctx.fail(validation.error)
-    if (!(await isPbRunning())) return ctx.fail("Client is not running")
-    
-    try {
-        const res = await pb.collection('users').authWithPassword(credentials!.email, credentials!.password, { requestKey: null })
-        return ctx.pass({token:res.token, record: res.record})
-    } catch (err) {
-        return ctx.fail(err)
-    }
-})
+	const credentials = ctx.fetch('my_credentials') as Credentials;
+
+	const validation = credentialsSchema.safeParse(credentials);
+	if (!validation.success) return ctx.fail(validation.error);
+	if (!(await isPbRunning())) return ctx.fail('Client is not running');
+
+	try {
+		const res = await pb
+			.collection('users')
+			.authWithPassword(credentials!.email, credentials!.password, { requestKey: null });
+		return ctx.pass({ token: res.token, record: res.record });
+	} catch (err) {
+		return ctx.fail(err);
+	}
+});
 
 /**
  * @internal
  */
-export const getList = p.new(['list_parameters'], 'ask records', async (ctx) =>{
-    const params = ctx.fetch('list_parameters') as ListParameters
-    const validation = listParametersSchema.safeParse(params)
-    if (!validation.success) return ctx.fail(validation.error)
+export const getList = p.new(['list_parameters'], 'ask records', async (ctx) => {
+	const params = ctx.fetch('list_parameters') as ListParameters;
+	const validation = listParametersSchema.safeParse(params);
+	if (!validation.success) return ctx.fail(validation.error);
 
-    const {collection, sort, filter, expand, type, requestKey } = params
-    if (!(await isPbRunning())) return ctx.fail("client is not working")
-    
-    const options:FullListOptions = { requestKey: requestKey || type }
-    if (sort) options.sort = sort
-    if (filter) options.filter = filter
-    if (expand) options['expand'] = expand
-    
-    let res: RecordModel | RecordModel[] | ListResult<RecordModel>
-    if (type === "all") {
-        res = await pb.collection(collection).getFullList(options)
-    }
-    else if (type === "list") {
-        const {page, perPage } = params.pagination
-        res = await pb.collection(collection).getList(page, perPage, options)
-    }
-    else {
-        res = await pb.collection(collection).getFirstListItem(filter, options)
-    }
-    //@ts-expect-error JsonableObject should take also ListResult
-    return ctx.pass({records:res})
-})
+	const { collection, sort, filter, expand, type, requestKey } = params;
+	if (!(await isPbRunning())) return ctx.fail('client is not working');
+
+	const options: FullListOptions = { requestKey: requestKey || type };
+	if (sort) options.sort = sort;
+	if (filter) options.filter = filter;
+	if (expand) options['expand'] = expand;
+
+	let res: RecordModel | RecordModel[] | ListResult<RecordModel>;
+	if (type === 'all') {
+		res = await pb.collection(collection).getFullList(options);
+	} else if (type === 'list') {
+		const { page, perPage } = params.pagination;
+		res = await pb.collection(collection).getList(page, perPage, options);
+	} else {
+		res = await pb.collection(collection).getFirstListItem(filter, options);
+	}
+	//@ts-expect-error JsonableObject should take also ListResult
+	return ctx.pass({ records: res });
+});
 
 /**
  * @internal
  */
 export const showRecord = p.new(['show_parameters'], 'ask record', async (ctx) => {
-    const p = ctx.fetch('show_parameters') as ShowRecordParameters
-    const validation = showParametersSchema.safeParse(p)
-    if (!validation.success) return ctx.fail(validation.error)
+	const p = ctx.fetch('show_parameters') as ShowRecordParameters;
+	const validation = showParametersSchema.safeParse(p);
+	if (!validation.success) return ctx.fail(validation.error);
 
-    const { expand, fields, requestKey} = p
-    const options: RecordOptions = {}
-    if (expand) options.expand = expand
-    if (fields) options.fields = fields
-    if (requestKey) options.requestKey = requestKey
-    try {
-        const res = await pb.collection(p.collection).getOne(p.id, options)
-        return ctx.pass(res)
-    } catch (err) {
-        return ctx.fail(err)
-    }
-})
+	const { expand, fields, requestKey } = p;
+	const options: RecordOptions = {};
+	if (expand) options.expand = expand;
+	if (fields) options.fields = fields;
+	if (requestKey) options.requestKey = requestKey;
+	try {
+		const res = await pb.collection(p.collection).getOne(p.id, options);
+		return ctx.pass(res);
+	} catch (err) {
+		return ctx.fail(err);
+	}
+});
 
 /**
  * @internal
  */
-export const createRecord = p.new(['create_parameters', 'record_parameters'], 'create record', async (ctx) => {
-    const p = ctx.fetch('create_parameters') as CreateRecordParameters
-    const r = ctx.fetch('record_parameters') as RecordBaseParameters
-    const validateCreateParams = createRecordParametersSchema.safeParse(p)
-    const validateRecordParams = baseRecordParametersSchema.safeParse(r)
-    if (!validateCreateParams.success) return ctx.fail(validateCreateParams.error)
-    if (!validateRecordParams.success) return ctx.fail(validateRecordParams.error)
+export const createRecord = p.new(
+	['create_parameters', 'record_parameters'],
+	'create record',
+	async (ctx) => {
+		const p = ctx.fetch('create_parameters') as CreateRecordParameters;
+		const r = ctx.fetch('record_parameters') as RecordBaseParameters;
 
-    const { expand, fields, requestKey } = r
-    const {collection, record } = p
-    
-    const options: RecordOptions = {}
-    if (expand) options.expand = expand
-    if (fields) options.fields = fields
-    if (requestKey) options.requestKey = requestKey
-    try {
-        const res = await pb.collection(collection).create(record, options)
-        return ctx.pass(res)
-    } catch (err) {
-        return ctx.fail(err.message)
-    }
-})
+		const validateCreateParams = createRecordParametersSchema.safeParse(p);
+		const validateRecordParams = baseRecordParametersSchema.safeParse(r);
+		if (!validateCreateParams.success) return ctx.fail(validateCreateParams.error);
+		if (!validateRecordParams.success) return ctx.fail(validateRecordParams.error);
 
-export const pocketbase = p
+		const { expand, fields, requestKey } = r;
+		const { collection, record } = p;
+
+		const options: RecordOptions = {};
+		if (expand) options.expand = expand;
+		if (fields) options.fields = fields;
+		if (requestKey) options.requestKey = requestKey;
+		try {
+			const res = await pb.collection(collection).create(record, options);
+			return ctx.pass(res);
+		} catch (err) {
+			return ctx.fail(err.message);
+		}
+	},
+);
+
+export const pocketbase = p;
--- pkg/pocketbase/test/e2e.ts
+++ pkg/pocketbase/test/e2e.ts
@@ -1,190 +1,195 @@
-import { ServerUrl, ListParameters, pocketbase, CreateRecordParameters, Credentials, RecordBaseParameters } from '@slangroom/pocketbase';
+import {
+	ServerUrl,
+	ListParameters,
+	pocketbase,
+	CreateRecordParameters,
+	Credentials,
+	RecordBaseParameters,
+} from '@slangroom/pocketbase';
 import test from 'ava';
 import { Slangroom } from '@slangroom/core';
 import { ShowRecordParameters } from '../src/plugin.js';
 
-const email = 'p@p.pp'
-const password = 'pppppppp'
+const email = 'p@p.pp';
+const password = 'pppppppp';
 
 test('should create a new slangroom client', async (t) => {
-    const script = `
+	const script = `
     Rule unknown ignore
     Given I send pb_address 'pb_address' and create pb_client and output into 'res'
     Given I have a 'string' named 'res'
     Then print data
-    `
-    const slangroom = new Slangroom(pocketbase)
-    const res = await slangroom.execute(script, {
+    `;
+	const slangroom = new Slangroom(pocketbase);
+	const res = await slangroom.execute(script, {
 		data: {
-			pb_address: 'http://127.0.0.1:8090/'
+			pb_address: 'http://127.0.0.1:8090/',
 		},
 	});
-    t.is(res.result['res'], 'pb client successfully created')
+	t.is(res.result['res'], 'pb client successfully created');
 });
 
 test('should login with credentials', async (t) => {
-    
-    const script = `
+	const script = `
     Rule unknown ignore
     Given I send pb_address 'pb_address' and create pb_client
     Given I send my_credentials 'my_credentials' and login and output into 'output'
     Given I have a 'string dictionary' named 'output'
     Then print data
-    `
-    const slangroom = new Slangroom(pocketbase)
-    const res = await slangroom.execute(script, {
+    `;
+	const slangroom = new Slangroom(pocketbase);
+	const res = await slangroom.execute(script, {
 		data: {
 			pb_address: 'http://127.0.0.1:8090/',
-            my_credentials: {
-                email,
-                password
-            }
+			my_credentials: {
+				email,
+				password,
+			},
 		},
 	});
-    const output = res.result['output'] as {record?: {email?:string}, token?:string}
-    t.is(output.record?.email, email)
-    t.truthy(output.token)
-})
+	const output = res.result['output'] as { record?: { email?: string }; token?: string };
+	t.is(output.record?.email, email);
+	t.truthy(output.token);
+});
 
 test('should retrieve full list of records', async (t) => {
-    const script = `
+	const script = `
     Rule unknown ignore
     Given I send pb_address 'pb_address' and create pb_client
     Given I send list_parameters 'list_parameters' and ask records and output into 'output'
     Given I have a 'string dictionary' named 'output'
     Then print data
-    `
-    const slangroom = new Slangroom(pocketbase)
+    `;
+	const slangroom = new Slangroom(pocketbase);
 
-    const data:{ pb_address:ServerUrl, list_parameters: ListParameters} = {
-        pb_address: 'http://127.0.0.1:8090/',
-        list_parameters: {
-            type: 'all',
-            collection: 'organizations'
-        }
-    }
-    const res = await slangroom.execute(script, {
-		data
+	const data: { pb_address: ServerUrl; list_parameters: ListParameters } = {
+		pb_address: 'http://127.0.0.1:8090/',
+		list_parameters: {
+			type: 'all',
+			collection: 'organizations',
+		},
+	};
+	const res = await slangroom.execute(script, {
+		data,
 	});
-    t.truthy(res.result)
-})
+	t.truthy(res.result);
+});
 
 test('should retrieve paginated list of records', async (t) => {
-    const script = `
+	const script = `
     Rule unknown ignore
     Given I send pb_address 'pb_address' and create pb_client
     Given I send list_parameters 'list_parameters' and ask records and output into 'output'
     Given I have a 'string dictionary' named 'output'
     Then print data
-    `
-    const slangroom = new Slangroom(pocketbase)
+    `;
+	const slangroom = new Slangroom(pocketbase);
 
-    const data:{ pb_address:ServerUrl, list_parameters: ListParameters} = {
-        pb_address: 'http://127.0.0.1:8090/',
-        list_parameters: {
-            type: 'list',
-            pagination:{
-                page:2,
-                perPage:20
-            },
-            collection: 'organizations'
-        }
-    }
-    const res = await slangroom.execute(script, {
-		data
+	const data: { pb_address: ServerUrl; list_parameters: ListParameters } = {
+		pb_address: 'http://127.0.0.1:8090/',
+		list_parameters: {
+			type: 'list',
+			pagination: {
+				page: 2,
+				perPage: 20,
+			},
+			collection: 'organizations',
+		},
+	};
+	const res = await slangroom.execute(script, {
+		data,
 	});
-    t.truthy(res.result)
-})
+	t.truthy(res.result);
+});
 
 test('should retrieve first record that match filters', async (t) => {
-    const script = `
+	const script = `
     Rule unknown ignore
     Given I send pb_address 'pb_address' and create pb_client
     Given I send list_parameters 'list_parameters' and ask records and output into 'output'
     Given I have a 'string dictionary' named 'output'
     Then print data
-    `
-    const slangroom = new Slangroom(pocketbase)
+    `;
+	const slangroom = new Slangroom(pocketbase);
 
-    const data:{ pb_address:ServerUrl, list_parameters: ListParameters} = {
-        pb_address: 'http://127.0.0.1:8090/',
-        list_parameters: {
-            type: 'first',
-            collection: 'organizations',
-            filter: 'created >= "2022-01-01 00:00:00"',
-            sort: '-created'
-        }
-    }
-    const res = await slangroom.execute(script, {
-		data
+	const data: { pb_address: ServerUrl; list_parameters: ListParameters } = {
+		pb_address: 'http://127.0.0.1:8090/',
+		list_parameters: {
+			type: 'first',
+			collection: 'organizations',
+			filter: 'created >= "2022-01-01 00:00:00"',
+			sort: '-created',
+		},
+	};
+	const res = await slangroom.execute(script, {
+		data,
 	});
-    //@ts-expect-error mm bib
-    console.log(res.result['output']!.records);
-    t.truthy(res.result)
-})
+	//@ts-expect-error mm bib
+	console.log(res.result['output']!.records);
+	t.truthy(res.result);
+});
 
 test('should retrieve one record', async (t) => {
-    const script = `
+	const script = `
     Rule unknown ignore
     Given I send pb_address 'pb_address' and create pb_client
     Given I send show_parameters 'show_parameters' and ask record and output into 'output'
     Given I have a 'string dictionary' named 'output'
     Then print data
-    `
-    const slangroom = new Slangroom(pocketbase)
+    `;
+	const slangroom = new Slangroom(pocketbase);
 
-    const data:{ pb_address:ServerUrl, show_parameters: ShowRecordParameters} = {
-        pb_address: 'http://127.0.0.1:8090/',
-        show_parameters: {
-            collection: 'organizations',
-            id: 'ouja6pwgxuyn2sd',
-            fields: 'name',
-        }
-    }
-    const res = await slangroom.execute(script, {
-		data
+	const data: { pb_address: ServerUrl; show_parameters: ShowRecordParameters } = {
+		pb_address: 'http://127.0.0.1:8090/',
+		show_parameters: {
+			collection: 'organizations',
+			id: 'ouja6pwgxuyn2sd',
+			fields: 'name',
+		},
+	};
+	const res = await slangroom.execute(script, {
+		data,
 	});
-    console.log(res.result['output']);
-    t.truthy(res.result)
-})
+	console.log(res.result['output']);
+	t.truthy(res.result);
+});
 
 test('should create a record', async (t) => {
-    const randomString = (Math.random() + 1).toString(36).substring(7);
+	const randomString = (Math.random() + 1).toString(36).substring(7);
 
-    const script = `
+	const script = `
     Rule unknown ignore
     Given I send pb_address 'pb_address' and create pb_client
     Given I send my_credentials 'my_credentials' and login
     Given I send create_parameters 'create_parameters' and send record_parameters 'record_parameters' and create record and output into 'output'
     Given I have a 'string dictionary' named 'output'
     Then print data
-    `
-    const slangroom = new Slangroom(pocketbase)
+    `;
+	const slangroom = new Slangroom(pocketbase);
 
-    type Data = { pb_address:ServerUrl, 
-        create_parameters: CreateRecordParameters, 
-        my_credentials:Credentials, 
-        record_parameters:RecordBaseParameters
-    }
-    const data: Data = {
-        pb_address: 'http://127.0.0.1:8090/',
-        create_parameters: {
-            collection: 'organizations',
-            record: {
-                name: `test-${randomString}`
-            },
-        },
-        record_parameters: {
-        },
-        my_credentials: {
-            email,
-            password
-        }
-    }
-    const res = await slangroom.execute(script, {
-		data
+	type Data = {
+		pb_address: ServerUrl;
+		create_parameters: CreateRecordParameters;
+		my_credentials: Credentials;
+		record_parameters: RecordBaseParameters;
+	};
+	const data: Data = {
+		pb_address: 'http://127.0.0.1:8090/',
+		create_parameters: {
+			collection: 'organizations',
+			record: {
+				name: `test-${randomString}`,
+			},
+		},
+		record_parameters: {},
+		my_credentials: {
+			email,
+			password,
+		},
+	};
+	const res = await slangroom.execute(script, {
+		data,
 	});
-    t.truthy(res.result)
-    t.truthy(true)
-})
-
+	t.truthy(res.result);
+	t.truthy(true);
+});
