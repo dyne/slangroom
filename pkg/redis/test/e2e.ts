f520b377ecfa2884b82deea496144744155f12f6
srfsh <dev@srf.sh>
2023-12-12 17:31:28 +0300

    test!(redis): fix encoding of number to string
    Due to recent update to Zenroom, the way 'string dictionary' works has changed,
    and now it encodes numbers as strings as well, so we apply the change here too.

--- pkg/redis/test/e2e.ts
+++ pkg/redis/test/e2e.ts
@@ -5,8 +5,8 @@ import { redis } from '@slangroom/redis';
 test('Redis write and read back', async (t) => {
 	const obj = {
 		name: 'test person',
-		age: Math.floor(Math.random() * 100),
-	}
+		age: Math.floor(Math.random() * 100).toString(),
+	};
 	const writeRedis = `
 Rule unknown ignore
 Given I connect to 'redis' and send key 'key1' and send object 'object1' and write object into key in redis
@@ -20,12 +20,11 @@ Then I connect to 'redis' and send key 'key1' and read key from redis and output
 	const slangroom = new Slangroom(redis);
 	const res = await slangroom.execute(writeRedis, {
 		keys: {
-			redis: "redis://localhost:6379",
+			redis: 'redis://localhost:6379',
 			object1: obj,
-			key1: 'persona'
+			key1: 'persona',
 		},
 	});
-	t.deepEqual(res["result"]["read1"], obj);
-	t.deepEqual(res["result"]["read2"], {});
+	t.deepEqual(res['result']['read1'], obj);
+	t.deepEqual(res['result']['read2'], {});
 });
-
