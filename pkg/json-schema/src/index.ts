a72432fcde347635ed421df15a5f4d44698fa6b6
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2023-12-13 17:19:57 +0100

    feat(plugins): add JSON validation schema plugin (#62)
    * chore: add root node_modules to gitignore
    
    * feat: setup json folder
    
    * feat(json): add ajv
    
    * feat(json): fixed plugin and tests
    
    * chore: package rename
    
    * Revert "chore: package rename"
    
    This reverts commit efe196563b537eca916cbd2c40adde945d14a0af.
    
    * chore: rename module
    
    * fix: removed commands
    
    ---------
    
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>

---
+++ pkg/json-schema/src/index.ts
@@ -0,0 +1,1 @@
+export * from '@slangroom/json-schema/plugin';
---
+++ pkg/json-schema/test/e2e.ts
@@ -0,0 +1,81 @@
+import test from 'ava';
+import { Slangroom } from '@slangroom/core';
+import { JSONSchema, SENTENCE_VALIDATE_JSON, ValidationError } from '@slangroom/json-schema';
+
+const KEY_DATA = 'data';
+const KEY_SCHEMA = 'schema';
+const KEY_OUTPUT = 'out';
+const SENTENCE = SENTENCE_VALIDATE_JSON(KEY_DATA, KEY_SCHEMA, KEY_OUTPUT);
+
+test('validate valid data with valid schema', async (t) => {
+	const script = `
+	Rule unknown ignore
+
+	${SENTENCE}
+	Then print data
+	`;
+
+	const slangroom = new Slangroom(JSONSchema);
+
+	const res = await slangroom.execute(script, {
+		data: {
+			[KEY_DATA]: 'nice',
+			[KEY_SCHEMA]: {
+				type: 'string',
+			},
+		},
+	});
+
+	const output = res.result[KEY_OUTPUT] as unknown as { errors: ValidationError[] };
+	t.deepEqual(output.errors, []);
+});
+
+test('validate invalid data with valid schema', async (t) => {
+	const script = `
+	Rule unknown ignore
+
+	${SENTENCE}
+	Then print data
+	`;
+
+	const slangroom = new Slangroom(JSONSchema);
+
+	const res = await slangroom.execute(script, {
+		data: {
+			[KEY_DATA]: 12,
+			[KEY_SCHEMA]: {
+				type: 'string',
+			},
+		},
+	});
+
+	const output = res.result[KEY_OUTPUT] as unknown as { errors: ValidationError[] };
+	t.true(Array.isArray(output.errors) && output.errors.length > 0);
+});
+
+test('load invalid schema', async (t) => {
+	const script = `
+	Rule unknown ignore
+
+	${SENTENCE}
+	Then print data
+	`;
+
+	const slangroom = new Slangroom(JSONSchema);
+
+	let error;
+	try {
+		await slangroom.execute(script, {
+			data: {
+				[KEY_DATA]: 12,
+				[KEY_SCHEMA]: {
+					invalid_key: 'string',
+				},
+			},
+		});
+	} catch (e) {
+		error = 'Invalid JSON schema';
+	}
+
+	t.assert(error);
+});
