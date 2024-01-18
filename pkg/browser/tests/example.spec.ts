9469f96df899c1c18d709368477c96c83356d21c
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-01-18 10:49:45 +0100

    feat(browser): add json-schema to browser plugin (#66)
    * feat: add json-schema to browser plugin
    
    * test(json-schema): add test

--- pkg/browser/tests/example.spec.ts
+++ pkg/browser/tests/example.spec.ts
@@ -7,4 +7,8 @@ test('check results of slangroom', async ({ page }) => {
 	await expect(page.locator('#test1')).toContainText(
 		'did:dyne:sandbox.test:pEn78CGNEKvMR7DJQ1yvUVUpAHKzsBz45mQw3zD2js9',
 	);
+
+	await expect(page.locator('#test-json-schema')).toContainText(
+		"{\"out\":{\"errors\":[]}}",
+	);
 });
