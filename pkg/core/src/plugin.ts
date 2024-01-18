beeee8567ca171ce437c705c87ba1823dba17828
Sir Deniz III <dev@srf.sh>
2024-01-18 12:49:13 +0300

    docs(core): Document the core module (#60)
    * feat(core): rm unused file
    
    * doc(core): add docs to lexer
    
    * doc(core): add docs to parser
    
    * doc(core): add docs to plugin
    
    * doc(core): add docs to slangroom
    
    * doc(core): add docs to visitor
    
    * doc(core): add docs to the package
    
    * doc(core): fix nitpicks of the package
    
    ---------
    
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>

--- pkg/core/src/plugin.ts
+++ pkg/core/src/plugin.ts
@@ -1,12 +1,65 @@
 import type { Jsonable } from '@slangroom/shared';
 import type { Ast } from '@slangroom/core';
 
+/**
+ * The representation of a plugin definition.  It is also used as a key in
+ * {@link PluginMap}.
+ */
 export type PluginMapKey = {
+	/**
+	 * The phrase of the plugin.
+	 *
+	 * @example
+	 * The `love asche` of the supposed plugin definition
+	 * ```
+	 * Given I send howmuch 'quantity' and love asche.
+	 * ```
+	 * is the phrase part.
+	 *
+	 * It must only be composed of alpha-numerical values with underscores or
+	 * dashes in between them (let's call them "words" here), and those "words"
+	 * must be split by whitespace (ensured by the plugin definition as just a
+	 * single blank space).
+	 */
 	phrase: string;
+
+	/**
+	 * An unordered list of parameters of a plugin.
+	 *
+	 * @example
+	 * The `object` and `header` of the supposed plugin definition
+	 * ```
+	 * Given I connect to 'url' and send object 'obj' and send headers 'hdr' and do http post request
+	 * ```
+	 * are the example of parameters of that definition.  A plugin might not
+	 * have any parameters.
+	 */
 	params?: string[];
+
+	/**
+	 * Whether a plugin definition uses Open or Connect, or not.
+	 *
+	 * @example
+	 * The `connect to 'url'` of the plugin definiton
+	 * ```
+	 * Given I connect to 'url' and send http get request
+	 * ```
+	 * is the Connect.
+	 *
+	 * @example
+	 * The `open 'url'` of the plugin definiton
+	 * ```
+	 * Given I open 'path' and read from file
+	 * ```
+	 * is the Open.
+	 */
 	openconnect?: 'open' | 'connect';
 };
 
+/**
+ * An error indicating that in a {@link PluginMap}, there exist more than a
+ * single unique {@link PluginMapKey}.
+ */
 export class DuplicatePluginError extends Error {
 	constructor({ openconnect, params, phrase }: PluginMapKey) {
 		super(
@@ -18,9 +71,33 @@ export class DuplicatePluginError extends Error {
 	}
 }
 
+/**
+ * A custom map implementation that uses {@link PluginMapKey} for the key parts,
+ * and {@link PluginExecutor} for the value parts.
+ *
+ * @remarks
+ * The reason we needed a custom implementation of a map is because of the fact
+ * that that {@link Map} class in JS uses the reference values of objects to
+ * check for uniqueness, not the fields and values of those fields in an object,
+ * which is a sensible default in a JS sensible manner, but doesn't work for us...
+ */
 export class PluginMap {
+	/**
+	 * The datastore backing our implementation of a map.
+	 *
+	 * We use an array for the implementation, and each method mutating this
+	 * array ensures that only a single unique value of {@link PluginMapKey}
+	 * exists in the entire array.
+	 */
 	#store: [PluginMapKey, PluginExecutor][] = [];
 
+	/**
+	 * Finds the index of the value inside the {@link #store}, where
+	 * {@link their} key matches with our's.
+	 *
+	 * @param their Their {@link PluginMapKey} to match against ours.
+	 * @returns the index of the match if found, or `-1` otherwise.
+	 */
 	#index(their: PluginMapKey) {
 		return this.#store.findIndex(([our]) => {
 			const openconn = their.openconnect === our.openconnect;
@@ -32,38 +109,174 @@ export class PluginMap {
 		});
 	}
 
+	/**
+	 * Loops over each value of the map, with a callback function taking a
+	 * single argument of an array of pairs of {@link PluginMapKey} and
+	 * {@link PluginExecutor}, respectively.
+	 *
+	 * @privateRemarks
+	 * I don't know why I can't just use `forEach = this.#store.forEach`, but it
+	 * just doesn't work.  So I have to define it this way, which provides a
+	 * limited set of what's possible with {@link #store.forEach}.
+	 */
 	forEach(cb: (value: [PluginMapKey, PluginExecutor]) => void) {
 		this.#store.forEach(cb);
 	}
 
+	/**
+	 * If exists, looks up the {@link PluginExecutor} referenced by {@link k}.
+	 *
+	 * @param k The key to look up with.
+	 *
+	 * @returns the {@link PluginExecutor} if exists.
+	 */
 	get(k: PluginMapKey) {
 		return this.#store[this.#index(k)]?.[1];
 	}
 
+	/**
+	 * Checks whether the given key {@link k} exists in the map.
+	 *
+	 * @param k The key to look up with.
+	 *
+	 * @returns Whether the key exists or not.
+	 */
 	has(k: PluginMapKey) {
 		return this.#index(k) !== -1;
 	}
 
+	/**
+	 * Sets the value {@link v} using the key {@link v}.
+	 *
+	 * @param k The key to point to the value {@link v}.
+	 * @param v The value to be pointed to by the key {@link k}.
+	 *
+	 * @throws {@link DuplicatePluginError}
+	 * If the key {@link k} already exists in the map.
+	 */
 	set(k: PluginMapKey, v: PluginExecutor) {
 		if (this.has(k)) throw new DuplicatePluginError(k);
 		this.#store.push([k, v]);
 	}
 }
 
+/**
+ * A callback function taking a single argument, which is called the "context",
+ * that is used to decide what to do with the plugin (access parameters, make
+ * the execution pass or fail, etc.).
+ *
+ * @example
+ * ```ts
+ * const lowercasify = (ctx) => {
+ * 	const foo = ctx.fetch("foo");
+ * 	if (typeof foo !== "string")
+ * 		return ctx.fail("foo must be string");
+ * 	return ctx.pass(foo.toLowerCase());
+ * };
+ * ```
+ */
 export type PluginExecutor = (ctx: PluginContext) => PluginResult | Promise<PluginResult>;
 
+/**
+ * Collects each custom implementation of a statement under a single unit, which
+ * is called a "plugin".
+ *
+ * @remarks
+ * A plugin can contain one or many implementations of custom statements, which
+ * are unique in the same definition of the plugin.
+ */
 export class Plugin {
-	store = new PluginMap();
+	/**
+	 * The list of plugin definitions belonging to this plugin.
+	 */
+	readonly store = new PluginMap();
 
+	/**
+	 * Defines a new plugin that has only a phrase.
+	 *
+	 * @param phrase The phrase of the definition.
+	 * @param executor The callback function to be ran when the plugin
+	 * definition matches with a given line.
+	 *
+	 * @throws {@link Error}
+	 * If {@link phrase} is ill-formed.
+	 *
+	 * @throws {@link DuplicatePluginError}
+	 * If the definition is duplicated.
+	 */
 	new(phrase: string, executor: PluginExecutor): PluginExecutor;
+
+	/**
+	 * Defines a new plugin that has a list of parameters along with a phrase.
+	 *
+	 * @param params The list of parameters to be needed by this plugin.
+	 * @param phrase The phrase of the definition.
+	 * @param executor The callback function to be ran when the plugin
+	 * definition matches with a given line.
+	 *
+	 * @throws {@link Error}
+	 * If any value of {@link params} is ill-formed.
+	 *
+	 * @throws {@link Error}
+	 * If {@link params} has duplicated entries.
+	 *
+	 * @throws {@link Error}
+	 * If {@link phrase} is ill-formed.
+	 *
+	 * @throws {@link DuplicatePluginError}
+	 * If the definition is duplicated.
+	 */
 	new(params: string[], phrase: string, executor: PluginExecutor): PluginExecutor;
+
+	/**
+	 * Defines a new plugin that has an open or connect along with a phrase.
+	 *
+	 * @param openconnect Whether this definition uses open or connect.
+	 * @param phrase The phrase of the definition.
+	 * @param executor The callback function to be ran when the plugin
+	 * definition matches with a given line.
+	 *
+	 * @throws {@link Error}
+	 * If {@link phrase} is ill-formed.
+	 *
+	 * @throws {@link DuplicatePluginError}
+	 * If the definition is duplicated.
+	 */
 	new(openconnect: 'open' | 'connect', phrase: string, executor: PluginExecutor): PluginExecutor;
+
+	/**
+	 * Defines a new plugin that has an open or connect along with a list of
+	 * params and a phrase.
+	 *
+	 * @param openconnect Whether this definition uses open or connect.
+	 * @param params The list of parameters to be needed by this plugin.
+	 * @param phrase The phrase of the definition.
+	 * @param executor The callback function to be ran when the plugin
+	 * definition matches with a given line.
+	 *
+	 * @throws {@link Error}
+	 * If any value of {@link params} is ill-formed.
+	 *
+	 * @throws {@link Error}
+	 * If {@link params} has duplicated entries.
+	 *
+	 * @throws {@link Error}
+	 * If {@link phrase} is ill-formed.
+	 *
+	 * @throws {@link DuplicatePluginError}
+	 * If the definition is duplicated.
+	 */
 	new(
 		openconnect: 'open' | 'connect',
 		params: string[],
 		phrase: string,
 		executor: PluginExecutor,
 	): PluginExecutor;
+
+	/**
+	 * The concerete implementation of the many overridden {@link new} methods.
+	 * See them for their documentation.
+	 */
 	new(
 		phraseOrParamsOrOpenconnect: string | string[] | 'open' | 'connect',
 		executorOrPhraseOrParams: PluginExecutor | string | string[],
@@ -166,14 +379,22 @@ export const isSane = (str: string) => /^[a-z][a-z0-9]*([_-][a-z0-9]+)*$/.test(s
 // Todo: Maybe we should adapt some sort of monad library.
 
 /**
- * Result of a Plugin execution.
+ * Result of a plugin execution.
  */
 export type PluginResult = ResultOk | ResultErr;
+
+/**
+ * Result of a plugin execution, indicating success.
+ */
 export type ResultOk = { ok: true; value: Jsonable };
+
+/**
+ * Result of a plugin execution, indicating failure.
+ */
 export type ResultErr = { ok: false; error: any };
 
 /**
- * The Plugin Context.  It has every info a Plugin needs, plus some utilities.
+ * The Plugin Context.  It has every info a plugin needs, plus some utilities.
  */
 export type PluginContext = {
 	/**
