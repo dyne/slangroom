f7dea33ac342e586fb97d64b09bcaeff6f603a50
puria <puria@dyne.org>
2024-02-26 18:20:07 +0100

    feat: allow missing conf and extra in zencode chaiz

--- pkg/zencode/src/plugin.ts
+++ pkg/zencode/src/plugin.ts
@@ -1,20 +1,43 @@
-import { Plugin  } from '@slangroom/core';
+import { Plugin } from '@slangroom/core';
 import type { JsonableObject } from '@slangroom/shared';
 import { zencodeExec } from '@slangroom/shared';
 
 const p = new Plugin();
 
+const exec = async (script: string, data: JsonableObject, keys: JsonableObject, extra: JsonableObject, conf: string = '') => {
+	if (!script) throw new Error('script is required');
+	if (!data) throw new Error('data is required');
+	if (!keys) throw new Error('keys is required');
+
+	return await zencodeExec(script, { data, keys, extra, conf });
+}
+
 /**
  * @internal
  */
-export const zencodeExecPlugin = p.new(['script', 'data', 'keys', 'extra', 'conf'], 'execute zencode', async (ctx) => {
+export const zencodeExecPlugin = p.new(['script', 'data', 'keys' ], 'execute zencode', async (ctx) => {
+	const script = ctx.fetch('script') as string;
+	const data = (ctx.get('data') || {}) as JsonableObject;
+	const keys = (ctx.get('keys') || {}) as JsonableObject;
+	const extra = {} as JsonableObject;
+	const conf = "";
+
+	const zout = await exec(script, data, keys, extra, conf);
+
+	return ctx.pass(zout.result)
+});
+
+/**
+ * @internal
+ */
+export const zencodeExecFullPlugin = p.new(['script', 'data', 'keys', 'extra', 'conf'], 'execute zencode', async (ctx) => {
 	const script = ctx.fetch('script') as string;
 	const data = (ctx.get('data') || {}) as JsonableObject;
 	const keys = (ctx.get('keys') || {}) as JsonableObject;
 	const extra = (ctx.get('extra') || {}) as JsonableObject;
 	const conf = (ctx.get('conf') || "") as string;
 
-	const zout = await zencodeExec(script, { data, keys, extra, conf	});
+	const zout = await exec(script, data, keys, extra, conf);
 
 	return ctx.pass(zout.result)
 });
