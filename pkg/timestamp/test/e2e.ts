0ea8018297d1f5e3dbb029c53434ffb58d3326ea
matteo-cristino <cristino.matteo97@gmail.com>
2024-02-29 21:25:36 +0100

    test(timestamp): finalize e2e tests

--- pkg/timestamp/test/e2e.ts
+++ pkg/timestamp/test/e2e.ts
@@ -5,23 +5,25 @@ import { timestamp } from '@slangroom/timestamp';
 test('fetch timestamp in seconds', async (t) => {
 	const contract = `Rule unknown ignore
 Given I fetch the local timestamp in seconds and output into 'output'
-Given I have a 'string' named 'output'
+Given I have a 'time' named 'output'
 Then print data
 `
     const sl = new Slangroom(timestamp);
 	const res = await sl.execute(contract);
 	const ts = new Date().getTime() / 1000;
-	t.true(Math.abs(Number(res) - ts) < 100);
+	t.truthy(res.result['output']);
+	t.true(Math.abs(Number(res.result['output']) - ts) < 100);
 });
 
 test('fetch timestamp in milliseconds', async (t) => {
 	const contract = `Rule unknown ignore
-Given I fetch the local timestamp in seconds and output into 'output'
+Given I fetch the local timestamp in milliseconds and output into 'output'
 Given I have a 'string' named 'output'
 Then print data
 `
     const sl = new Slangroom(timestamp);
 	const res = await sl.execute(contract);
 	const ts = new Date().getTime();
-	t.true(Math.abs(Number(res) - ts) < 100);
+	t.truthy(res.result['output']);
+	t.true(Math.abs(Number(res.result['output']) - ts) < 100000);
 });
