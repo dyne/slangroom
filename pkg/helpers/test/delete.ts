1d9680c44b5d9f9d1003d49ef2e301ebb8de20cb
matteo-cristino <cristino.matteo97@gmail.com>
2024-03-06 17:21:44 +0100

    feat(helpers): statment to delete an element

---
+++ pkg/helpers/test/delete.ts
@@ -0,0 +1,30 @@
+import { Slangroom } from '@slangroom/core';
+import { helpers } from '@slangroom/helpers';
+import test from 'ava';
+
+test('@slangroom/helpers 🧹 delete', async (t) => {
+	const setter = `Rule unknown ignore
+Given I have a 'string dictionary' named 'the_object'
+Given I have a 'string dictionary' named 'mimmo'
+
+Then print 'mimmo'
+Then print 'the_object'
+Then I manipulate and delete and output into 'mimmo'
+`;
+	const slangroom = new Slangroom(helpers);
+	const res = await slangroom.execute(setter, {
+		data: {
+			"mimmo": {
+				"a": "b"
+			},
+			"the_object": {
+				"name": "mimmo"
+			},
+		},
+	});
+	t.deepEqual(res.result, {
+		the_object: {
+			name: 'mimmo',
+		},
+	}, res.logs)
+});
