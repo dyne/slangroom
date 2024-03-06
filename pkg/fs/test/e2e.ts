8fe956ed2fdf3e9f1dc6d16189748af6d4a71b7a
Matteo Cristino <102997993+matteo-cristino@users.noreply.github.com>
2024-03-06 13:05:01 +0100

    feat(fs): statement to verify if a file exist or not (#98)
    Co-authored-by: Puria Nafisi Azizi <puria@dyne.org>

--- pkg/fs/test/e2e.ts
+++ pkg/fs/test/e2e.ts
@@ -47,3 +47,73 @@ test.serial('Read verbatim file', async (t) => {
         filename: "test.txt"
     });
 });
+
+test.serial('path not a string', async (t) => {
+	process.env['FILES_DIR'] = "./test";
+	const slangroom = new Slangroom(fs);
+	const fn = slangroom.execute(READ, {
+		data: {
+			filename: {
+				"path": "test.txt"
+			}
+		},
+	});
+	const error = await t.throwsAsync(fn);
+	t.is((error as Error).message, 'path must be string');
+});
+
+test.serial('File exists', async (t) => {
+	const verifyExists = `Rule unknown ignore
+Given I send path 'filename' and verify file exists
+Given I have a 'string' named 'filename'
+Then print the 'filename'
+Then print the string 'the file exists'
+`
+	process.env['FILES_DIR'] = "./test";
+	const slangroom = new Slangroom(fs);
+	const resultExists = slangroom.execute(verifyExists, {
+		data: {
+			filename: "test.txt"
+		},
+	});
+	const res = await resultExists;
+	t.deepEqual(res.result, {
+		filename: 'test.txt',
+		output: ["the_file_exists"]
+	});
+	const resultNotExists = slangroom.execute(verifyExists, {
+		data: {
+			filename: "test_not_exist.txt"
+		},
+	});
+	const error = await t.throwsAsync(resultNotExists);
+	t.is((error as Error).message, 'no such file or directory: test/test_not_exist.txt');
+});
+
+test.serial('File does not exist', async (t) => {
+	const verifyDoesNotExists = `Rule unknown ignore
+Given I send path 'filename' and verify file does not exist
+Given I have a 'string' named 'filename'
+Then print the 'filename'
+Then print the string 'the file does not exist'
+`
+	process.env['FILES_DIR'] = "./test";
+	const slangroom = new Slangroom(fs);
+	const resultExists = slangroom.execute(verifyDoesNotExists, {
+		data: {
+			filename: "test.txt"
+		},
+	});
+	const error = await t.throwsAsync(resultExists);
+	t.is((error as Error).message, 'file or directory found under: test/test.txt');
+	const resultNotExists = slangroom.execute(verifyDoesNotExists, {
+		data: {
+			filename: "test_not_exist.txt"
+		},
+	});
+	const res = await await resultNotExists;
+	t.deepEqual(res.result, {
+		filename: 'test_not_exist.txt',
+		output: ["the_file_does_not_exist"]
+	});
+});
