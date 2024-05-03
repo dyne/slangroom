8a92989d9e3d6f2db83debc8c6543fd4015b871a
matteo-cristino <cristino.matteo97@gmail.com>
2024-05-03 18:34:20 +0200

    fix(ouath): add _ prefix to parameter name not yet used in function to avoid unused ts error

--- pkg/oauth/src/authorizeHandler.ts
+++ pkg/oauth/src/authorizeHandler.ts
@@ -458,7 +458,7 @@ export class AuthorizeHandler {
 	 * Build an error response that redirects the user-agent to the client-provided url.
 	 */
 
-	buildErrorRedirectUri(redirectUri: string, error: Error) {
+	buildErrorRedirectUri(redirectUri: string, _error: Error) {
 		//TODO
 		const uri = url.parse(redirectUri);
 		// console.log(error);
