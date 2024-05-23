d33ea91a7afb1038a885b893519c1f1c3006d18f
matteo-cristino <cristino.matteo97@gmail.com>
2024-05-23 10:21:11 +0200

    fix(json-schema): bump ajv dep to 8.13.0 that now support direct use of constructor

--- pkg/json-schema/src/plugin.ts
+++ pkg/json-schema/src/plugin.ts
@@ -3,11 +3,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import { Plugin } from '@slangroom/core';
-import Ajv, { type ValidationError } from 'ajv';
+import { Ajv, type ValidationError } from 'ajv';
 // read the version from the package.json
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const packageJson = require('@slangroom/json-schema/package.json');
+import packageJson from '@slangroom/json-schema/package.json' assert { type: 'json' };
 
 export { ValidationError };
 
@@ -41,7 +39,7 @@ export const validateJSON = p.new(
 		const schema = ctx.fetch(ARG_JSON_SCHEMA);
 
 		try {
-			const ajv = new Ajv.default({ allErrors: true });
+			const ajv = new Ajv({ allErrors: true });
 
 			// @ts-ignore
 			const validate = ajv.compile(schema);
