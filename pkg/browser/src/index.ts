3163cd1202d6256b2b10dbe1bddbddf54dcf6b9a
Matteo Cristino <102997993+matteo-cristino@users.noreply.github.com>
2024-06-11 00:48:39 +0200

    feat(browser) add @slangroom/helpers pkg (#155)

--- pkg/browser/src/index.ts
+++ pkg/browser/src/index.ts
@@ -6,8 +6,9 @@ import { Slangroom } from '@slangroom/core';
 import { qrcode } from '@slangroom/qrcode';
 import { http } from '@slangroom/http';
 import { pocketbase } from '@slangroom/pocketbase';
+import { helpers } from '@slangroom/helpers';
 import {JSONSchema} from '@slangroom/json-schema'
-const slangroom = new Slangroom([http, qrcode, JSONSchema, pocketbase]);
+const slangroom = new Slangroom([http, qrcode, JSONSchema, pocketbase, helpers]);
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).slangroom = slangroom; // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513740/5433572
