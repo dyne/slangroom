850a1feeb4b641788d68668445d897b20fd71ad7
matteo-cristino <cristino.matteo97@gmail.com>
2024-06-26 15:28:49 +0200

    test(db): more failing test

--- pkg/db/test/e2e.ts
+++ pkg/db/test/e2e.ts
@@ -11,8 +11,10 @@ import fs from "fs";
 import packageJson from '@slangroom/db/package.json' with { type: 'json' };
 
 process.env['FILES_DIR'] = "./test";
-const dbPath1 = "sqlite://./test/db1.db";
-const dbPath2 = "sqlite://./test/db2.db";
+const fileDb1 = './test/db1.db';
+const fileDb2 = './test/db2.db';
+const dbPath1 = `sqlite://${fileDb1}`;
+const dbPath2 = `sqlite://${fileDb2}`;
 
 const stripAnsiCodes = (str: string) => str.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '');
 
@@ -36,16 +38,8 @@ const complexQuery = `Rule unknown ignore
 		`;
 
 test.afterEach(() => {
-	try {
-		fs.unlinkSync("./test/db1.db");
-	} catch (e) {
-		throw e;
-	}
-	try {
-		fs.unlinkSync("./test/db2.db");
-	} catch (e) {
-		throw e;
-	}
+	if (fs.existsSync(fileDb1)) fs.unlinkSync(fileDb1);
+	if (fs.existsSync(fileDb2)) fs.unlinkSync(fileDb2);
 });
 
 test.beforeEach(async () => {
@@ -165,46 +159,28 @@ test.serial(
 	}
 );
 
+const readQuery = `Rule unknown ignore
+Given I connect to 'db' and send record 'n' and send table 'myTable' and read the record of the table and output into 'result'
+Given I have a 'string dictionary' named 'result'
+Then print all data`;
+
 test.serial('Db should fail for wrong record', async (t) => {
-	const failQuery = `Rule unknown ignore
-		Scenario 'ecdh': Create the keypair
-		Given I connect to 'myDb2' and send variable 'obj_1' and send name 'var_name' and send table 'myCache' and save the variable in the database table
-		# the value of the record could be 0 to max could be
-		Given I connect to 'myDb1' and send record 'n' and send table 'myTable' and read the record of the table and output into 'myZenroomStringDictionary'
-		Given I am 'John'
-		Given I have a 'string' named 'myDb1'
-		Given I have a 'string' named 'myDb2'
-		Given I have a 'string' named 'myTable'
-		Given I have a 'string' named 'myCache'
-		Given I have a 'string dictionary' named 'myZenroomStringDictionary'
-		Then print all data
-		`;
 	const slangroom = new Slangroom(db);
-	const result = slangroom.execute(failQuery, {
+	const result = slangroom.execute(readQuery, {
 		data: {
 			"n": 30,
-			"var_name": "obj_1",
-			"obj_1": "pippo",
-			"myDb1": "sqlite://./test/db1.db",
-			"myDb2": "sqlite://./test/db2.db",
+			"db": "sqlite://./test/db1.db",
 			"myTable": "firstTable",
-			"myCache": "firstCache",
-			"myOtherZenroomStringDictionary": {
-				"data": {
-					"data1": "9WgBlK+Kcq3AKpmhituXQe4UPkzH3zpZiQa4Szm1Q40=",
-					"data2": "BCEo8MgRiSxtLfxE4UEDVnbdZ21h+xc+egLIRk3VTagpJxlBfu9MjqXGUi2N7tIewpcDrr5V7Z2cmMcNsbKWSGQ="
-				}
-			}
-		},
+		}
 	});
 
 	const error = await t.throwsAsync(result);
 	t.is(stripAnsiCodes((error as Error).message),
-		`3 |         # the value of the record could be 0 to max could be
-4 |         Given I connect to 'myDb1' and send record 'n' and send table 'myTable' and read the record of the table and output into 'myZenroomStringDictionary'
-            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-5 |         Given I am 'John'
-6 |         Given I have a 'string' named 'myDb1'
+		`0 | Rule unknown ignore
+1 | Given I connect to 'db' and send record 'n' and send table 'myTable' and read the record of the table and output into 'result'
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+2 | Given I have a 'string dictionary' named 'result'
+3 | Then print all data
 
 Error colors:
  - error
@@ -216,46 +192,23 @@ Slangroom @slangroom/db@${packageJson.version} Error: Returned null for id "30" 
 `);
 });
 
-test.serial('Db should fail for wrong db', async (t) => {
-	const failQuery = `Rule unknown ignore
-		Scenario 'ecdh': Create the keypair
-		# the value of the record could be 0 to max could be
-		Given I connect to 'myDb1' and send record 'n' and send table 'myTable' and read the record of the table and output into 'myZenroomStringDictionary'
-		Given I am 'John'
-		Given I have a 'string' named 'obj_1'
-		Given I have a 'string' named 'var_name'
-		Given I have a 'string' named 'myDb1'
-		Given I have a 'string' named 'myDb2'
-		Given I have a 'string' named 'myTable'
-		Given I have a 'string' named 'myCache'
-		Given I have a 'string dictionary' named 'myZenroomStringDictionary'
-		Then print all data
-		Then I connect to 'myDb2' and send variable 'obj_1' and send name 'var_name' and send table 'myCache' and save the variable in the database table
-		Then I connect to 'myTable' and send variable 'obj_1' and send name 'var_name' and send table 'myCache' and save the variable in the database table`;
+test.serial('Db should fail for wrong db dialect', async (t) => {
 	const slangroom = new Slangroom(db);
-	const result = slangroom.execute(failQuery, {
+	const result = slangroom.execute(readQuery, {
 		data: {
 			"n": 3,
-			"var_name": "obj_1",
-			"obj_1": "pippo",
-			"myDb1": "sqlite://./test/db1.db",
-			"myDb2": "sqlite://./test/db2.db",
 			"myTable": "firstTable",
-			"myCache": "firstCache",
-			"myOtherZenroomStringDictionary": {
-				"data": {
-					"data1": "9WgBlK+Kcq3AKpmhituXQe4UPkzH3zpZiQa4Szm1Q40=",
-					"data2": "BCEo8MgRiSxtLfxE4UEDVnbdZ21h+xc+egLIRk3VTagpJxlBfu9MjqXGUi2N7tIewpcDrr5V7Z2cmMcNsbKWSGQ="
-				}
-			}
+			"db": "mariodb://usr:pwd@example.com"
 		},
 	});
 
 	const error = await t.throwsAsync(result);
 	t.is(stripAnsiCodes((error as Error).message),
-		`13 |         Then I connect to 'myDb2' and send variable 'obj_1' and send name 'var_name' and send table 'myCache' and save the variable in the database table
-14 |         Then I connect to 'myTable' and send variable 'obj_1' and send name 'var_name' and send table 'myCache' and save the variable in the database table
-             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+		`0 | Rule unknown ignore
+1 | Given I connect to 'db' and send record 'n' and send table 'myTable' and read the record of the table and output into 'result'
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+2 | Given I have a 'string dictionary' named 'result'
+3 | Then print all data
 
 Error colors:
  - error
@@ -263,6 +216,34 @@ Error colors:
  - missing words
  - extra words
 
-Slangroom @slangroom/db@${packageJson.version} Error: Cannot read properties of null (reading 'replace')
+Slangroom @slangroom/db@${packageJson.version} Error: The dialect mariodb is not supported. Supported dialects: mssql, mariadb, mysql, oracle, postgres, db2 and sqlite.
+`);
+});
+
+test.serial('Db should fail with wrong db url', async (t) => {
+	const slangroom = new Slangroom(db);
+	const result = slangroom.execute(readQuery, {
+		data: {
+			"n": 3,
+			"myTable": "firstTable",
+			"db": "mariadb/user:pwdo@example.com"
+		},
+	});
+
+	const error = await t.throwsAsync(result);
+	t.is(stripAnsiCodes((error as Error).message),
+		`0 | Rule unknown ignore
+1 | Given I connect to 'db' and send record 'n' and send table 'myTable' and read the record of the table and output into 'result'
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+2 | Given I have a 'string dictionary' named 'result'
+3 | Then print all data
+
+Error colors:
+ - error
+ - suggested words
+ - missing words
+ - extra words
+
+Slangroom @slangroom/db@${packageJson.version} Error: Invalid URL
 `);
 });
