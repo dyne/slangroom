f954b2fa5fc87b58db72dd7e7e8a0580a9995ca0
matteo-cristino <cristino.matteo97@gmail.com>
2024-08-02 12:34:39 +0200

    refactor(browser): on start print the list of plugins along with the version

--- pkg/browser/src/index.ts
+++ pkg/browser/src/index.ts
@@ -3,14 +3,49 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import { Slangroom } from '@slangroom/core';
-import { qrcode } from '@slangroom/qrcode';
-import { http } from '@slangroom/http';
-import { pocketbase } from '@slangroom/pocketbase';
-import { helpers } from '@slangroom/helpers';
-import {JSONSchema} from '@slangroom/json-schema'
-const slangroom = new Slangroom([http, qrcode, JSONSchema, pocketbase, helpers]);
+import { qrcode, version as qrcodeVersion } from '@slangroom/qrcode';
+import { http, version as httpVersion } from '@slangroom/http';
+import { pocketbase, version as pocketbaseVersion } from '@slangroom/pocketbase';
+import { helpers, version as helpersVersion } from '@slangroom/helpers';
+import { JSONSchema, version as jsonSchemaVersion } from '@slangroom/json-schema'
+import packageJson from '@slangroom/browser/package.json' with { type: 'json' };
+
+export const version = packageJson.version;
+
+const plugins_dict = {
+	http: {
+		plugin: http,
+		version: httpVersion
+	},
+	qrcode: {
+		plugin: qrcode,
+		version: qrcodeVersion
+	},
+	pocketbase: {
+		plugin: pocketbase,
+		version: pocketbaseVersion
+	},
+	helpers: {
+		plugin: helpers,
+		version: helpersVersion
+	},
+	'json-schema': {
+		plugin: JSONSchema,
+		version: jsonSchemaVersion
+	}
+};
+
+let welcome_message = '🎉 Slangroom is ready, installed plugins:\n';
+
+const plugins = [];
+for (const [name, dict] of Object.entries(plugins_dict)) {
+	plugins.push(dict.plugin);
+	welcome_message = welcome_message.concat(` - @slangroom/${name}@${dict.version}\n`);
+}
+
+const slangroom = new Slangroom(plugins);
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).slangroom = slangroom; // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513740/5433572
 
-console.log('🎉 Slangroom is ready');
+console.log(welcome_message);
