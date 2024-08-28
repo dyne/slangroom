3142f3b691b5f42fadc63bc845a55846c9d45bee
Matteo Cristino <102997993+matteo-cristino@users.noreply.github.com>
2024-08-28 12:36:51 +0200

    feat: add slangroom grammar package (#196)
    * initial package structure
    
    * change example to slangroom
    
    * fix: scenario statement
    
    * update makefile
    
    * add first simple tests
    
    * fix: add License
    
    * fix: given have statement
    
    * fix: send can be optional
    
    * fix: ethereum actions
    
    * fix: Helpers send
    
    * update keywords list
    
    * update Given name statement
    
    * fix: add the possibility of having more than one scenario
    
    * update test list
    
    * add some others test from didroom
    
    * add grammar test to github actions
    
    * fix(grammar): add missing syntax.grammar.d.ts file
    
    * chore(grammar): remove package-lock since using pnpm
    
    * chore: add grammar to workspace
    
    * chore(grammar): remove capital letter from package name
    
    * chore: add test:grammar command to test grammar
    
    * ci: update grammar test
    
    * refactor(grammar): remove unused Makefile
    
    * chore: add missing license headers
    
    * chore: add grammar package acknowledgement in the readme
    
    * chore: Give to Cesar what is to Cesar
    
    * feat(grammar): publish npm package on main merge
    
    * pnpm-lock ❗
    
    ---------
    
    Co-authored-by: FilippoTrotter <filippotrotter99@gmail.com>
    Co-authored-by: puria <puria@dyne.org>

---
+++ grammar/src/index.ts
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2024 Dyne.org foundation
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import {parser} from "./syntax.grammar"
+import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
+import {styleTags, tags as t} from "@lezer/highlight"
+
+export const SlangroomLanguage = LRLanguage.define({
+  parser: parser.configure({
+    props: [
+      indentNodeProp.add({
+        Application: delimitedIndent({closing: ")", align: false})
+      }),
+      foldNodeProp.add({
+        Application: foldInside
+      }),
+      styleTags({
+        Identifier: t.variableName,
+        Boolean: t.bool,
+        String: t.string,
+        LineComment: t.lineComment,
+        "( )": t.paren
+      })
+    ]
+  }),
+  languageData: {
+    commentTokens: {line: ";"}
+  }
+})
+
+export function Slangroom() {
+  return new LanguageSupport(SlangroomLanguage)
+}
---
+++ grammar/test/test.js
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2024 Dyne.org foundation
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import {SlangroomLanguage} from "../dist/index.js"
+import {fileTests} from "@lezer/generator/dist/test"
+
+import * as fs from "fs"
+import * as path from "path"
+import { fileURLToPath } from 'url';
+let caseDir = path.dirname(fileURLToPath(import.meta.url))
+
+for (let file of fs.readdirSync(caseDir)) {
+  if (!/\.txt$/.test(file)) continue
+
+  let name = /^[^\.]*/.exec(file)[0]
+  describe(name, () => {
+    for (let {name, run} of fileTests(fs.readFileSync(path.join(caseDir, file), "utf8"), file))
+      it(name, () => run(SlangroomLanguage.parser))
+  })
+}
