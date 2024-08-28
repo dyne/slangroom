7a5ebd2b8f4d15bbf7c2a14ad93abb8885282c1e
puria <puria@dyne.org>
2024-08-28 13:21:29 +0200

    fix(grammar): add syntax highlighting for main phases

--- grammar/src/index.ts
+++ grammar/src/index.ts
@@ -2,33 +2,43 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import {styleTags, tags as t} from "@lezer/highlight"
+import { parser } from "./syntax.grammar"
+import { LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent } from "@codemirror/language"
+import { styleTags, tags as t } from "@lezer/highlight"
+import { completeFromList } from "@codemirror/autocomplete"
 
 export const SlangroomLanguage = LRLanguage.define({
-  parser: parser.configure({
-    props: [
-      indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
-      }),
-      foldNodeProp.add({
-        Application: foldInside
-      }),
-      styleTags({
-        Identifier: t.variableName,
-        Boolean: t.bool,
-        String: t.string,
-        LineComment: t.lineComment,
-        "( )": t.paren
-      })
-    ]
-  }),
-  languageData: {
-    commentTokens: {line: ";"}
-  }
+	parser: parser.configure({
+		props: [
+			indentNodeProp.add({
+				Application: delimitedIndent({ closing: ")", align: false })
+			}),
+			foldNodeProp.add({
+				Application: foldInside
+			}),
+			styleTags({
+				Identifier: t.variableName,
+				WhenStatement: t.variableName,
+				ThenStatement: t.bool,
+				Rule: t.string,
+				StringLitteral: t.string,
+				LineComment: t.lineComment
+			})
+		]
+	}),
+	languageData: {
+		commentTokens: { line: "#" }
+	}
+})
+
+export const ac = SlangroomLanguage.data.of({
+	autocomplete: completeFromList([
+		{ label: "given", type: "keyword" },
+		{ label: "then", type: "keyword" },
+		{ label: "when", type: "keyword" }
+	])
 })
 
 export function Slangroom() {
-  return new LanguageSupport(SlangroomLanguage)
+	return new LanguageSupport(SlangroomLanguage, [ac])
 }
