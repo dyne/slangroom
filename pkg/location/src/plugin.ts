82ab02200112e01377e66dce05067374623dff45
matteo-cristino <cristino.matteo97@gmail.com>
2024-10-08 11:03:24 +0200

    chore: prettier

--- pkg/location/src/plugin.ts
+++ pkg/location/src/plugin.ts
@@ -17,27 +17,28 @@ export class LocationBaseError extends Error {
 
 const p = new Plugin();
 
- const isBrowser = (ctx:PluginContext)=>{
-    if (window === undefined) {
-        return ctx.fail(new LocationBaseError('You must be in a browser environment to use this plugin'));
-    }
-    return undefined
-}
+const isBrowser = (ctx: PluginContext) => {
+	if (window === undefined) {
+		return ctx.fail(
+			new LocationBaseError('You must be in a browser environment to use this plugin'),
+		);
+	}
+	return undefined;
+};
 
-const isUrlAString = (ctx:PluginContext, url:unknown)=>{
-    if (typeof url !== 'string') {
-        return ctx.fail(new LocationBaseError('url must be a string'));
-    }
-    return undefined
-}
-
+const isUrlAString = (ctx: PluginContext, url: unknown) => {
+	if (typeof url !== 'string') {
+		return ctx.fail(new LocationBaseError('url must be a string'));
+	}
+	return undefined;
+};
 
 /**
  * @internal
  */
 export const getLocation = p.new('connect', 'get the current location', (ctx) => {
-    isBrowser(ctx)
-	const location = window.location
+	isBrowser(ctx);
+	const location = window.location;
 	return ctx.pass({
 		href: location.href,
 		protocol: location.protocol,
@@ -46,166 +47,164 @@ export const getLocation = p.new('connect', 'get the current location', (ctx) =>
 		port: location.port,
 		pathname: location.pathname,
 		search: location.search,
-		hash: location.hash
-	})
+		hash: location.hash,
+	});
 });
 
 /**
  * @internal
  */
 export const getHref = p.new('connect', 'get the current location href', (ctx) => {
-    isBrowser(ctx)
-    return ctx.pass(window.location.href)
+	isBrowser(ctx);
+	return ctx.pass(window.location.href);
 });
 
 /**
  * @internal
  */
 export const getProtocol = p.new('connect', 'get the current location protocol', (ctx) => {
-    isBrowser(ctx)
-    return ctx.pass(window.location.protocol)
+	isBrowser(ctx);
+	return ctx.pass(window.location.protocol);
 });
 
 /**
  * @internal
  */
 export const getHost = p.new('connect', 'get the current location host', (ctx) => {
-    isBrowser(ctx)
-    return ctx.pass(window.location.host)
+	isBrowser(ctx);
+	return ctx.pass(window.location.host);
 });
 
 /**
  * @internal
  */
 export const getHostname = p.new('connect', 'get the current location hostname', (ctx) => {
-    isBrowser(ctx)
-    return ctx.pass(window.location.hostname)
+	isBrowser(ctx);
+	return ctx.pass(window.location.hostname);
 });
 
 /**
  * @internal
  */
 export const getPort = p.new('connect', 'get the current location port', (ctx) => {
-    isBrowser(ctx)
-    return ctx.pass(window.location.port)
+	isBrowser(ctx);
+	return ctx.pass(window.location.port);
 });
 
 /**
  * @internal
  */
 export const getPathname = p.new('connect', 'get the current location pathname', (ctx) => {
-    isBrowser(ctx)
-    return ctx.pass(window.location.pathname)
+	isBrowser(ctx);
+	return ctx.pass(window.location.pathname);
 });
 
 /**
  * @internal
  */
 export const getSearch = p.new('connect', 'get the current location search params', (ctx) => {
-    isBrowser(ctx)
-    return ctx.pass(window.location.search)
+	isBrowser(ctx);
+	return ctx.pass(window.location.search);
 });
 
 /**
  * @internal
  */
 export const getHash = p.new('connect', 'get the current location hash', (ctx) => {
-    isBrowser(ctx)
-    return ctx.pass(window.location.hash)
+	isBrowser(ctx);
+	return ctx.pass(window.location.hash);
 });
 
 /**
  * @internal
  */
 export const reload = p.new('connect', 'reload the current page', (ctx) => {
-    isBrowser(ctx)
-    window.location.reload()
-    return ctx.pass('reload');
+	isBrowser(ctx);
+	window.location.reload();
+	return ctx.pass('reload');
 });
 
 /**
  * @internal
  */
 export const replace = p.new(['url'], 'replace the current location', (ctx) => {
-    isBrowser(ctx)
-    const url = ctx.get('url') as string;
-    isUrlAString(ctx,url)
-    window.location.replace(url)
-    return ctx.pass('replace');
+	isBrowser(ctx);
+	const url = ctx.get('url') as string;
+	isUrlAString(ctx, url);
+	window.location.replace(url);
+	return ctx.pass('replace');
 });
 
 /**
  * @internal
  */
 export const assign = p.new(['url'], 'assign the current location', (ctx) => {
-    isBrowser(ctx)
-    const url = ctx.get('url') as string;
-    isUrlAString(ctx,url)
-    window.location.assign(url)
-    return ctx.pass('assign');
+	isBrowser(ctx);
+	const url = ctx.get('url') as string;
+	isUrlAString(ctx, url);
+	window.location.assign(url);
+	return ctx.pass('assign');
 });
 
 /**
  * @internal
  */
 export const redirectToUrl = p.new(['url'], 'redirect to the url', (ctx) => {
-    isBrowser(ctx)
-    const url = ctx.get('url') as string;
-    isUrlAString(ctx,url)
-    window.location.href = url
-    return ctx.pass('redirect');
+	isBrowser(ctx);
+	const url = ctx.get('url') as string;
+	isUrlAString(ctx, url);
+	window.location.href = url;
+	return ctx.pass('redirect');
 });
 
 /**
  * @internal
  */
 export const goBack = p.new(['url'], 'go back in history', (ctx) => {
-    isBrowser(ctx)
-    window.history.back()
-    return ctx.pass('back');
+	isBrowser(ctx);
+	window.history.back();
+	return ctx.pass('back');
 });
 
 /**
  * @internal
  */
 export const goForward = p.new(['url'], 'go forward in history', (ctx) => {
-    isBrowser(ctx)
-    window.history.forward()
-    return ctx.pass('forward');
+	isBrowser(ctx);
+	window.history.forward();
+	return ctx.pass('forward');
 });
 
 /**
  * @internal
  */
 export const go = p.new(['index'], 'go to a specific page in history', (ctx) => {
-    isBrowser(ctx)
-    const index = ctx.get('index')
-    if (typeof index !== 'number') {
-        return ctx.fail(new LocationBaseError('index must be a number'))
-    }
-    window.history.go(index)
-    return ctx.pass('go');
+	isBrowser(ctx);
+	const index = ctx.get('index');
+	if (typeof index !== 'number') {
+		return ctx.fail(new LocationBaseError('index must be a number'));
+	}
+	window.history.go(index);
+	return ctx.pass('go');
 });
 
 /**
  * @internal
  */
 export const length = p.new('connect', 'get the history length', (ctx) => {
-    isBrowser(ctx)
-    return ctx.pass(window.history.length)
+	isBrowser(ctx);
+	return ctx.pass(window.history.length);
 });
-
-
 
 /**
  * @internal
  */
 export const openWindow = p.new(['url'], 'open the url in a new window', (ctx) => {
-    isBrowser(ctx)
-    const url = ctx.get('url') as string;
-    isUrlAString(ctx,url)
-    window.open(url, '_blank')
-    return ctx.pass('open');
+	isBrowser(ctx);
+	const url = ctx.get('url') as string;
+	isUrlAString(ctx, url);
+	window.open(url, '_blank');
+	return ctx.pass('open');
 });
 
-export const location = p
+export const location = p;
