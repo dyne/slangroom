0022a851de63d37631013a881dc2348c89471402
matteo-cristino <cristino.matteo97@gmail.com>
2024-11-04 11:49:28 +0100

    chore: add missing license header

--- pkg/oauth/test/start_microservices.sh
+++ pkg/oauth/test/start_microservices.sh
@@ -1,4 +1,7 @@
-#!/bin/env bash
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2024 Dyne.org foundation
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
 temp_dir="pkg/oauth/test/didroom_microservices"
 
