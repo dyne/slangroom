0c4ff34dfd40126e64fca34e2cc7bee7d76cf75d
matteo-cristino <cristino.matteo97@gmail.com>
2025-04-15 16:12:36 +0200

    feat: first draft for autogenerated grammar syntax
    improved strictness on grammar, but at the same time we lost highlight of the core part of the statement. Need some time more to understand how to get both. Moreover this cover only old format, once it is ready I should include also new format

--- grammar/utils/prepare_complete.mjs
+++ grammar/utils/prepare_complete.mjs
@@ -24,31 +24,68 @@ import { shell } from "@slangroom/shell";
 import { timestamp } from "@slangroom/timestamp";
 import { wallet } from "@slangroom/wallet";
 import { zencode } from "@slangroom/zencode";
+import { pbkdf2 } from 'crypto';
 
 const fullStatementTemplates = [];
 
+let pluginSpecificStatements = '// ===== Plugin-Specific Statements =====\n';
+let pluginActions = '// ===== Plugin-Actions =====\n';
+const SlangroomPluginsStatements = [];
+
+const permutations = (arr) => {
+	if (arr.length <= 1) return [arr];
+
+	const result = [];
+	for (let i = 0; i < arr.length; i++) {
+		const current = arr[i];
+		const remaining = arr.slice(0, i).concat(arr.slice(i + 1));
+		const perms = permutations(remaining);
+		for (const perm of perms) {
+			result.push([current, ...perm]);
+		}
+	}
+	return result;
+}
+
 const generateStatements = (nameAndPlugin) => {
 	const [name, plugin] = nameAndPlugin;
+	const nameLowerCase = name.toLowerCase();
 	const p = new Slangroom(plugin).getPlugin();
+	const pluginStatement = `${name}Statement`;
+	const pluginAction = `${name}Action`;
+	SlangroomPluginsStatements.push(pluginStatement);
+	const pluginStatementsTable = [];
+	const pluginActionsTable = [];
 	p.forEach(([k]) => {
 		let openConnect = '';
 		let sendParams = '';
 		let withParams = '';
 		let whereParams = '';
+		let statementGrammar = '';
 		if (k.openconnect) {
 			if (k.openconnect === 'connect') {
 				openConnect = `connect to '' and `;
+				statementGrammar = `connect to StringLiteral and `;
 			} else if (k.openconnect === 'open') {
 				openConnect = `open '' and `;
+				statementGrammar = `open StringLiteral and `;
 			}
 		}
+		let sends = [];
 		if (k.params) {
+			if (k.params.length > 1) statementGrammar += `( `;
 			k.params.forEach((param) => {
-				sendParams = sendParams.concat(`send ${param} '' and `);
-				withParams = withParams.concat(`${param} '', `);
-				whereParams = whereParams.concat(`${param} is '', `);
+				sends.push(`send ${param} StringLiteral and `);
+				sendParams += `send ${param} '' and `;
+				withParams += `${param} '', `;
+				whereParams += `${param} is '', `;
 			})
+			statementGrammar += permutations(sends).map((perm) => perm.join('')).join('|');
+			if (k.params.length > 1) statementGrammar += `) `;
 		}
+		statementGrammar += `${k.phrase} (and? SaveAction)*`;
+		pluginStatementsTable.push(statementGrammar);
+		pluginActionsTable.push(k.phrase);
 
 		withParams = withParams.slice(0, -2);
 		whereParams = whereParams.slice(0, -2);
@@ -57,39 +94,43 @@ const generateStatements = (nameAndPlugin) => {
 		const whereStatement = `${openConnect}${k.phrase} where ${whereParams}`;
 		const lowerCaseStatement = `I ${openConnect}${sendParams.toLowerCase()}${k.phrase.toLowerCase()}`;
 		fullStatementTemplates.push(
-			{ label: `${name} given ${lowerCaseStatement}`, displayLabel:`Given ${statement}`, type: "keyword", info: `[${name}]` },
-			{ label: `${name} prepare: ${withStatement.toLowerCase()}`, displayLabel:`Prepare: ${withStatement}`, type: "keyword", info: `[${name}]` },
-			{ label: `${name} prepare: ${whereStatement.toLowerCase()}`, displayLabel:`Prepare: ${whereStatement}`, type: "keyword", info: `[${name}]` },
-			{ label: `${name} then ${lowerCaseStatement}`, displayLabel: `Then ${statement}`, type: "keyword", info: `[${name}]` },
-			{ label: `${name} compute: ${withStatement.toLowerCase()}`, displayLabel:`Compute: ${withStatement}`, type: "keyword", info: `[${name}]` },
-			{ label: `${name} compute: ${whereStatement.toLowerCase()}`, displayLabel:`Compute: ${whereStatement}`, type: "keyword", info: `[${name}]` },
-			{ label: `${name} given ${lowerCaseStatement} and output into ''`, displayLabel: `Given ${statement} and output into ''`, type: "keyword", info: `[${name}]` },
-			{ label: `${name} prepare '': ${withStatement.toLowerCase()}`, displayLabel:`Prepare '': ${withStatement}`, type: "keyword", info: `[${name}]` },
-			{ label: `${name} prepare '': ${whereStatement.toLowerCase()}`, displayLabel:`Prepare '': ${whereStatement}`, type: "keyword", info: `[${name}]` },
-			{ label: `${name} then ${lowerCaseStatement} and output into ''`, displayLabel: `Then ${statement} and output into ''`, type: "keyword", info: `[${name}]` },
-			{ label: `${name} compute '': ${withStatement.toLowerCase()}`, displayLabel:`Compute '': ${withStatement}`, type: "keyword", info: `[${name}]` },
-			{ label: `${name} compute '': ${whereStatement.toLowerCase()}`, displayLabel:`Compute '': ${whereStatement}`, type: "keyword", info: `[${name}]` },
+			{ label: `${nameLowerCase} given ${lowerCaseStatement}`, displayLabel: `Given ${statement}`, type: "keyword", info: `[${name}]` },
+			{ label: `${nameLowerCase} prepare: ${withStatement.toLowerCase()}`, displayLabel: `Prepare: ${withStatement}`, type: "keyword", info: `[${name}]` },
+			{ label: `${nameLowerCase} prepare: ${whereStatement.toLowerCase()}`, displayLabel: `Prepare: ${whereStatement}`, type: "keyword", info: `[${name}]` },
+			{ label: `${nameLowerCase} then ${lowerCaseStatement}`, displayLabel: `Then ${statement}`, type: "keyword", info: `[${name}]` },
+			{ label: `${nameLowerCase} compute: ${withStatement.toLowerCase()}`, displayLabel: `Compute: ${withStatement}`, type: "keyword", info: `[${name}]` },
+			{ label: `${nameLowerCase} compute: ${whereStatement.toLowerCase()}`, displayLabel: `Compute: ${whereStatement}`, type: "keyword", info: `[${name}]` },
+			{ label: `${nameLowerCase} given ${lowerCaseStatement} and output into ''`, displayLabel: `Given ${statement} and output into ''`, type: "keyword", info: `[${name}]` },
+			{ label: `${nameLowerCase} prepare '': ${withStatement.toLowerCase()}`, displayLabel: `Prepare '': ${withStatement}`, type: "keyword", info: `[${name}]` },
+			{ label: `${nameLowerCase} prepare '': ${whereStatement.toLowerCase()}`, displayLabel: `Prepare '': ${whereStatement}`, type: "keyword", info: `[${name}]` },
+			{ label: `${nameLowerCase} then ${lowerCaseStatement} and output into ''`, displayLabel: `Then ${statement} and output into ''`, type: "keyword", info: `[${name}]` },
+			{ label: `${nameLowerCase} compute '': ${withStatement.toLowerCase()}`, displayLabel: `Compute '': ${withStatement}`, type: "keyword", info: `[${name}]` },
+			{ label: `${nameLowerCase} compute '': ${whereStatement.toLowerCase()}`, displayLabel: `Compute '': ${whereStatement}`, type: "keyword", info: `[${name}]` },
 		);
 	});
+	pluginSpecificStatements += `\n${pluginStatement} {\n    ${pluginStatementsTable.join(' |\n    ')}\n}`;
+	pluginActions += `\n${pluginAction} {\n    ${pluginActionsTable.join(' |\n    ')}\n}`;
 }
 
 [
-	['db', db],
-	['ethereum', ethereum],
-	['fs', fs],
-	['git', git],
-	['helpers', helpers],
-	['http', http],
-	['JSONSchema', JSONSchema],
-	['oauth', oauth],
-	['pocketbase', pocketbase],
-	['qrcode', qrcode],
-	['rdf', rdf],
-	['redis', redis],
-	['shell', shell],
-	['timestamp', timestamp],
-	['wallet', wallet],
-	['zencode', zencode]
-].map((x) => generateStatements(x))
+	['Db', db],
+	['Ethereum', ethereum],
+	['Fs', fs],
+	['Git', git],
+	['Helpers', helpers],
+	['Http', http],
+	['JsonSchema', JSONSchema],
+	['OAuth', oauth],
+	['Pocketbase', pocketbase],
+	['QrCode', qrcode],
+	['Rdf', rdf],
+	['Redis', redis],
+	['Shell', shell],
+	['Timestamp', timestamp],
+	['Wallet', wallet],
+	['Zencode', zencode]
+].forEach((x) => generateStatements(x))
 
 await pfs.writeFile('../src/complete_statement.ts', `export const fullStatementTemplates = ${JSON.stringify(fullStatementTemplates, null, 4)}`, 'utf-8')
+const syntaxGrammar = await pfs.readFile('./syntax.grammar.template', 'utf-8');
+await pfs.writeFile('../src/syntax.grammar', syntaxGrammar.replace("{{ Plugin-Specific Statements }}", pluginSpecificStatements).replace("{{ Plugin-Actions }}",pluginActions), 'utf-8');
