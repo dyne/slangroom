d09e51d1a2a23473a72d0a28815604b469153753
matteo-cristino <cristino.matteo97@gmail.com>
2025-05-10 16:31:06 +0200

    test(ethereum): reactivate some tests
    deploy the storage smart contract and reactivate first tests, still need to add the erc20 smart contract to reactivate the others

--- pkg/ethereum/test/plugin.ts
+++ pkg/ethereum/test/plugin.ts
@@ -20,8 +20,8 @@ import {
 
 const test = ava as TestFn<{ web3: Web3 }>;
 
-test.skip('read the ethereum nonce', async (t) => {
-	const ctx = new PluginContextTest('http://78.47.38.223:9485', {
+test('read the ethereum nonce', async (t) => {
+	const ctx = new PluginContextTest('http://localhost:9485', {
 		address: '0x2D010920b43aFb54f8d5fB51c9354FbC674b28Fc',
 	});
 	const res = await ethNonce(ctx);
@@ -31,8 +31,8 @@ test.skip('read the ethereum nonce', async (t) => {
 	});
 });
 
-test.skip('Ethereum gas price', async (t) => {
-	const ctx = PluginContextTest.connect('http://78.47.38.223:9485');
+test('Ethereum gas price', async (t) => {
+	const ctx = PluginContextTest.connect('http://localhost:9485');
 	const res = await ethGasPrice(ctx);
 	t.truthy(res.ok);
 	if (res.ok) t.is(typeof res.value, 'string');
@@ -41,7 +41,7 @@ test.skip('Ethereum gas price', async (t) => {
 test.skip('Retrieve a zenroom object', async (t) => {
 	const poem =
 		'000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000674e656c206d657a7a6f2064656c2063616d6d696e206469206e6f7374726120766974610a6d6920726974726f7661692070657220756e612073656c7661206f73637572612c0a6368c3a9206c612064697269747461207669612065726120736d6172726974612e00000000000000000000000000000000000000000000000000';
-	const ctx = new PluginContextTest('http://78.47.38.223:9485', {
+	const ctx = new PluginContextTest('http://localhost:9485', {
 		transaction_id: '0x0467636a2557a1ccdaf10ce17ee74340096c510acfa9181c85756d43a8bed522',
 	});
 	const res = await ethBytes(ctx);
@@ -51,8 +51,8 @@ test.skip('Retrieve a zenroom object', async (t) => {
 	});
 });
 
-test.skip('Ethereum balance', async (t) => {
-	const ctx = new PluginContextTest('http://78.47.38.223:9485', {
+test('Ethereum balance', async (t) => {
+	const ctx = new PluginContextTest('http://localhost:9485', {
 		address: '0x2D010920b43aFb54f8d5fB51c9354FbC674b28Fc',
 	});
 	const res = await ethBalanceAddr(ctx);
@@ -62,8 +62,8 @@ test.skip('Ethereum balance', async (t) => {
 	});
 });
 
-test.skip('Read the balance of an array of addresses', async (t) => {
-	const ctx = new PluginContextTest('http://78.47.38.223:9485', {
+test('Read the balance of an array of addresses', async (t) => {
+	const ctx = new PluginContextTest('http://localhost:9485', {
 		addresses: [
 			'0xc32510251F77382bb9214144D2c488408Ec2047C',
 			'0xFf02577F140557190693cFf549025e66119FEA52',
@@ -76,7 +76,7 @@ test.skip('Read the balance of an array of addresses', async (t) => {
 });
 
 test.skip('erc20 symbol()', async (t) => {
-	const ctx = new PluginContextTest('http://78.47.38.223:9485', {
+	const ctx = new PluginContextTest('http://localhost:9485', {
 		sc: '0x720F72765775bb85EAAa08BB74442F106d3ffA03',
 	});
 	const res = await erc20symbol(ctx);
@@ -87,7 +87,7 @@ test.skip('erc20 symbol()', async (t) => {
 });
 
 test.skip('erc20 name()', async (t) => {
-	const ctx = new PluginContextTest('http://78.47.38.223:9485', {
+	const ctx = new PluginContextTest('http://localhost:9485', {
 		sc: '0x720F72765775bb85EAAa08BB74442F106d3ffA03',
 	});
 	const res = await erc20name(ctx);
@@ -98,7 +98,7 @@ test.skip('erc20 name()', async (t) => {
 });
 
 test.skip('erc20 totalSupply()', async (t) => {
-	const ctx = new PluginContextTest('http://78.47.38.223:9485', {
+	const ctx = new PluginContextTest('http://localhost:9485', {
 		sc: '0x720F72765775bb85EAAa08BB74442F106d3ffA03',
 	});
 	const res = await erc20totalSupply(ctx);
@@ -109,7 +109,7 @@ test.skip('erc20 totalSupply()', async (t) => {
 });
 
 test.skip('erc20 decimals()', async (t) => {
-	const ctx = new PluginContextTest('http://78.47.38.223:9485', {
+	const ctx = new PluginContextTest('http://localhost:9485', {
 		sc: '0x720F72765775bb85EAAa08BB74442F106d3ffA03',
 	});
 	const res = await erc20decimals(ctx);
@@ -121,7 +121,7 @@ test.skip('erc20 decimals()', async (t) => {
 
 test.skip('erc20 with invalid address', async (t) => {
 	const sc = '0x720765775bb85EAAa08BB74442F106d3ffA03';
-	const ctx = new PluginContextTest('http://78.47.38.223:9485', { sc: sc });
+	const ctx = new PluginContextTest('http://localhost:9485', { sc: sc });
 
 	const res = await erc20symbol(ctx);
 	t.deepEqual(res, {
@@ -136,7 +136,7 @@ test.skip('erc20 with invalid address', async (t) => {
 		t.fail(errors?.toString());
 		return;
 	}
-	const ctx = new PluginContextTest('http://78.47.38.223:9485', {
+	const ctx = new PluginContextTest('http://localhost:9485', {
 		sc: "0x720F72765775bb85EAAa08BB74442F106d3ffA03",
 		address: "0x7d6df85bDBCe99151c813fd1DDE6BC007c523C27"
 	});
